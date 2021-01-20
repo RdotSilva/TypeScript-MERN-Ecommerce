547d544a8e203ad28cf2d33cf47c5bf6f12085f8
RdotSilva <RyandotSilva@gmail.com>
2021-01-19 19:19:52 -0500

    Add Navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 interface Props {}
 
 const Header = (props: Props) => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">MERN ECommerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
