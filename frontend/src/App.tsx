d9b89cf4edd2d29ea98416e5f1604eaf96fec5f2
RdotSilva <RyandotSilva@gmail.com>
2021-01-22 18:48:29 -0500

    Add route for ProductScreen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const App: React.FC = () => {
       <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
