ad49f08746042703d1c3f9f251a89d980a5b8f6b
RdotSilva <RyandotSilva@gmail.com>
2021-01-21 20:07:03 -0500

    Add Router to main App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -6,15 +7,15 @@ import HomeScreen from "./screens/HomeScreen";
 
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
