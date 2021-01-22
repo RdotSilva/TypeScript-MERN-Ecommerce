61fa2c4421d71108e9850bd9284578c6ad10d6a1
RdotSilva <RyandotSilva@gmail.com>
2021-01-22 18:49:48 -0500

    Replace a tags w/ Link

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { ProductType } from "../types";
 import Rating from "./Rating";
 
@@ -10,16 +11,16 @@ interface Props {
 const Product = ({ product }: Props) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
           <Rating
