b7924525e656f34c72b4669f4d7e2c4a86e109d2
RdotSilva <RyandotSilva@gmail.com>
2021-01-21 19:56:51 -0500

    Add key to list item

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -12,7 +12,7 @@ const HomeScreen = (props: Props) => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product: ProductType) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
