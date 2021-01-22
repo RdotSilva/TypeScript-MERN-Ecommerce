6acf56a40c9fbea3bf0ab232d9b3cf29b9e5a8cd
RdotSilva <RyandotSilva@gmail.com>
2021-01-21 19:49:29 -0500

    Add star images

--- frontend/src/components/Rating.tsx
+++ frontend/src/components/Rating.tsx
@@ -6,7 +6,66 @@ interface Props {
 }
 
 const Rating = ({ value, text }: Props) => {
-  return <div className="rating"></div>;
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
 };
 
 export default Rating;
