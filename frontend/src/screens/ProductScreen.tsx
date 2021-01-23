366f85a325bc53599fbe0de30441bd67090821e8
RdotSilva <RyandotSilva@gmail.com>
2021-01-23 13:05:34 -0500

    Add ListGroup Items for product

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Col, Image, Row } from "react-bootstrap";
+import { Col, Image, ListGroup, Row } from "react-bootstrap";
 import { Link, RouteComponentProps } from "react-router-dom";
+import Rating from "../components/Rating";
 import products from "../products";
 import { ProductType } from "../types";
 
@@ -23,9 +24,25 @@ const ProductScreen = ({ match }: Props) => {
         </Link>
         <Row>
           <Col md={6}>
-            <Image src={product.image} alt={product.name} />
+            <Image src={product.image} alt={product.name} fluid />
           </Col>
-          <Col md={3}></Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
         </Row>
       </>
     );
