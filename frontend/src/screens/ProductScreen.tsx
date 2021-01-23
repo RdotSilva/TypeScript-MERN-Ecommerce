238c68d052c43163ce9b00147a56746e881b9997
RdotSilva <RyandotSilva@gmail.com>
2021-01-22 19:13:05 -0500

    Add product image

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { Col, Image, Row } from "react-bootstrap";
+import { Link, RouteComponentProps } from "react-router-dom";
 import products from "../products";
+import { ProductType } from "../types";
 
 interface MatchParams {
   id: string;
@@ -10,7 +12,24 @@ interface Props extends RouteComponentProps<MatchParams> {}
 
 const ProductScreen = ({ match }: Props) => {
   const product = products.find((product) => product._id === match.params.id);
-  return <div>Product</div>;
+
+  if (product === undefined) {
+    return <>No Product Data</>;
+  } else {
+    return (
+      <>
+        <Link className="btn btn-light my-3" to="/">
+          Go Back
+        </Link>
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} />
+          </Col>
+          <Col md={3}></Col>
+        </Row>
+      </>
+    );
+  }
 };
 
 export default ProductScreen;
