4a9df8fdfde267f709e3afc7d47247795482a28b
RdotSilva <RyandotSilva@gmail.com>
2021-01-24 19:57:13 -0500

    Add route for single product

--- backend/server.ts
+++ backend/server.ts
@@ -11,4 +11,9 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
 app.listen(5000, console.log(`Server running on port 5000`));
