54e5015d796524b0e3b9ef50e8c0f49fee91257e
RdotSilva <RyandotSilva@gmail.com>
2021-01-24 19:56:31 -0500

    Add products route

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,14 @@
 const express = require("express");
+const products = require("./data/products");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API IS RUNNING...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
 app.listen(5000, console.log(`Server running on port 5000`));
