56d3e132468acb2d49abdf17ab0c3c2681f5a282
RdotSilva <RyandotSilva@gmail.com>
2021-01-27 18:58:22 -0500

    Add connectDB method

---
+++ backend/config/db.ts
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
