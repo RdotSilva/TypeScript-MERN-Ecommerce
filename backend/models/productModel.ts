dded0a44857a3200ab0412cf10e084770d03d218
RdotSilva <RyandotSilva@gmail.com>
2021-01-27 19:14:48 -0500

    Add productSchema

---
+++ backend/models/productModel.ts
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User", // Reference the User Model for the Object ID (add relationship between product and user)
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // TODO: Add the rest of the product fields
+  },
+  { timestamps: true } // Automatically create "createdAt timestamp"
+);
