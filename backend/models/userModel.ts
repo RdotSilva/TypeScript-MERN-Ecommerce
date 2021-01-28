0c9cbadacc991ee5a1d5b01816313fcc03dd902a
RdotSilva <RyandotSilva@gmail.com>
2021-01-27 19:12:07 -0500

    Add userSchema

---
+++ backend/models/userModel.ts
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true } // Automatically create "createdAt timestamp"
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
