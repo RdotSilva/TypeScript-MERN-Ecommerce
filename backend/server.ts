9691f902c7df9936de49a807646ca3e4f95ac889
RdotSilva <RyandotSilva@gmail.com>
2021-01-29 18:49:34 -0500

    Add types and change imports

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,9 @@
-const express = require("express");
-const products = require("./data/products");
-const dotenv = require("dotenv");
+import express from "express";
+import products from "./data/products";
+import dotenv from "dotenv";
+import { Request, Response } from "express";
 import connectDB from "./config/db";
+import { ProductType } from "./types";
 
 dotenv.config();
 
@@ -9,22 +11,23 @@ const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("API IS RUNNING...");
 });
 
-app.get("/api/products", (req, res) => {
+app.get("/api/products", (req: Request, res: Response) => {
   res.json(products);
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
+app.get("/api/products/:id", (req: Request, res: Response) => {
+  const product = products.find(
+    (product: ProductType) => product._id === req.params.id
+  );
   res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
