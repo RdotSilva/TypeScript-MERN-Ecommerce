27c8c350de4c5720dbf091baf2ddcc012f3a8d67
RdotSilva <RyandotSilva@gmail.com>
2021-01-30 19:38:30 -0500

    Add hard coded user list

---
+++ backend/data/users.ts
@@ -0,0 +1,20 @@
+const users: any[] = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: "xxx",
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "John@Doe.com",
+    password: "xxx",
+    isAdmin: false,
+  },
+  {
+    name: "Jane Doe",
+    email: "Jane@Doe.com",
+    password: "xxx",
+    isAdmin: false,
+  },
+];
