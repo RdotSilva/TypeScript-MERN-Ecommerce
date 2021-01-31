6dffbcb63bd2c448b9a1d3597795d486a789bd00
RdotSilva <RyandotSilva@gmail.com>
2021-01-30 19:25:25 -0500

    Remove hard coded id

--- backend/data/products.ts
+++ backend/data/products.ts
@@ -2,7 +2,6 @@ import { ProductType } from "./../types";
 
 const products: ProductType[] = [
   {
-    _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
     image: "/images/airpods.jpg",
     description:
@@ -15,7 +14,6 @@ const products: ProductType[] = [
     numReviews: 12,
   },
   {
-    _id: "2",
     name: "iPhone 11 Pro 256GB Memory",
     image: "/images/phone.jpg",
     description:
@@ -28,7 +26,6 @@ const products: ProductType[] = [
     numReviews: 8,
   },
   {
-    _id: "3",
     name: "Cannon EOS 80D DSLR Camera",
     image: "/images/camera.jpg",
     description:
@@ -41,7 +38,6 @@ const products: ProductType[] = [
     numReviews: 12,
   },
   {
-    _id: "4",
     name: "Sony Playstation 4 Pro White Version",
     image: "/images/playstation.jpg",
     description:
@@ -54,7 +50,6 @@ const products: ProductType[] = [
     numReviews: 12,
   },
   {
-    _id: "5",
     name: "Logitech G-Series Gaming Mouse",
     image: "/images/mouse.jpg",
     description:
@@ -67,7 +62,6 @@ const products: ProductType[] = [
     numReviews: 10,
   },
   {
-    _id: "6",
     name: "Amazon Echo Dot 3rd Generation",
     image: "/images/alexa.jpg",
     description:
