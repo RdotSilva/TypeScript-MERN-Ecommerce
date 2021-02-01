1ebb9e3bae32db48fd7825e448f7442734e5ffb2
RdotSilva <RyandotSilva@gmail.com>
2021-02-01 18:56:00 -0500

    Update routes to fetch products from database

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -1,15 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 const router = express.Router();
+import Product from "../models/productModel";
 
-router.get("/", (req: Request, res: Response) => {
-  res.json(products);
-});
+router.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get("/:id", (req: Request, res: Response) => {
-  const product = products.find(
-    (product: ProductType) => product._id === req.params.id
-  );
-  res.json(product);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
 
 export default router;
