ffe22ab7d77f6ca4c0366d569fb0cbc12d785845
RdotSilva <RyandotSilva@gmail.com>
2021-02-01 18:59:23 -0500

    Add TSDoc comments

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -3,6 +3,11 @@ import asyncHandler from "express-async-handler";
 const router = express.Router();
 import Product from "../models/productModel";
 
+/**
+ * Fetch app products
+ * @route GET /api/products
+ * @access Public
+ */
 router.get(
   "/",
   asyncHandler(async (req: Request, res: Response) => {
@@ -11,6 +16,12 @@ router.get(
   })
 );
 
+/**
+ * Fetch single product
+ * @route GET /api/products/:id
+ * @param id ID of product to fetch
+ * @access Public
+ */
 router.get(
   "/:id",
   asyncHandler(async (req: Request, res: Response) => {
