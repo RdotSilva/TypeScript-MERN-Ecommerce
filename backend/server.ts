7b48be2d7454c466d422e08c1e1f25990deffa21
RdotSilva <RyandotSilva@gmail.com>
2021-02-01 19:23:20 -0500

    Add error handling middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
-import { Request, Response } from "express";
 import connectDB from "./config/db";
 import productRoutes from "./routes/productRoutes";
 
@@ -15,6 +14,15 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/products/", productRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const statusCode: number = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
