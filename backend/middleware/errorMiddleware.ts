beaba808d7bc3ea5158f472fe8082bcb7a32640f
RdotSilva <RyandotSilva@gmail.com>
2021-02-02 19:10:39 -0500

    Add errorHandler middleware

--- backend/middleware/errorMiddleware.ts
+++ backend/middleware/errorMiddleware.ts
@@ -10,3 +10,19 @@ const notFound = (
   res.status(404);
   next(error);
 };
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode: number = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
