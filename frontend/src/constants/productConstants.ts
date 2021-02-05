5eefe75447d3e15245a73260088bb72f12564c1c
RdotSilva <RyandotSilva@gmail.com>
2021-02-04 19:45:18 -0500

    Add TSDoc comments

--- frontend/src/constants/productConstants.ts
+++ frontend/src/constants/productConstants.ts
@@ -1,3 +1,6 @@
+/**
+ * Product List constants
+ */
 export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST";
 export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS";
 export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL";
