2e929394be6ede57d4d4c3ce13526c5534f0132b
RdotSilva <RyandotSilva@gmail.com>
2021-02-04 19:05:12 -0500

    Add listProducts action

---
+++ frontend/src/actions/productActions.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from "./../constants/productConstants";
+
+// Action creators
+export const listProducts = () => async (dispatch: Dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
