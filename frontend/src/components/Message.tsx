dd97b54d623ccbf5d9c62f7d0bb23a708f3ebeb7
RdotSilva <RyandotSilva@gmail.com>
2021-02-05 19:37:24 -0500

    Add Props interface

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
 
+interface Props {
+  variant: string;
+  children: string;
+}
+
 const Message = ({ variant, children }: Props) => {
   return <Alert variant={variant}>{children}</Alert>;
 };
