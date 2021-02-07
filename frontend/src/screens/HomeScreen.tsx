c649763756cb39e6577b9499f6d64659b9f9d7a8
RdotSilva <RyandotSilva@gmail.com>
2021-02-06 20:29:58 -0500

    Fix loading error

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
@@ -10,37 +10,36 @@ import { ProductType } from "../types";
 interface Props {}
 
 const HomeScreen = (props: Props) => {
-  const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
 
   const productList = useSelector((state: RootState) => state.productList);
 
-  /**
-   * Fetches all products from the backend
-   */
-  const fetchProducts = async () => {
-    dispatch(listProducts());
-
-    const { loading, error, products } = productList;
-
-    setProducts(products);
-  };
+  const {
+    loading,
+    error,
+    products,
+  }: { loading: boolean; error: Error; products: ProductType[] } = productList;
 
   useEffect(() => {
-    fetchProducts();
+    dispatch(listProducts());
   }, [dispatch]);
-
-  //TODO: Fix loading issue. Might have to temporarily remove loading text and revert back to previous code.
 
   return (
     <>
-      <Row>
-        {products.map((product: ProductType) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product: ProductType) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
