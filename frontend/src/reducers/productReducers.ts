68474288f572682d8fa2ef02660fb64d79608b58
RdotSilva <RyandotSilva@gmail.com>
2021-02-07 17:18:14 -0500

    Add productDetailsReducer

--- frontend/src/reducers/productReducers.ts
+++ frontend/src/reducers/productReducers.ts
@@ -18,3 +18,19 @@ export const productListReducer = (
       return state;
   }
 };
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action: ProductActionType
+) => {
+  switch (action.type) {
+    case "PRODUCT_DETAILS_REQUEST":
+      return { loading: true, ...state };
+    case "PRODUCT_DETAILS_SUCCESS":
+      return { loading: false, product: action.payload };
+    case "PRODUCT_DETAILS_FAIL":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
