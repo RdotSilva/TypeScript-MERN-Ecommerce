81e1453ba7b6a1c531341f2a1080bb2706c00e60
RdotSilva <RyandotSilva@gmail.com>
2021-02-06 19:51:31 -0500

    Add ProductListRequestAction interface

---
+++ frontend/src/types/actions.ts
@@ -0,0 +1,11 @@
+import { ProductType } from "./../types";
+
+export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST";
+export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS";
+export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL";
+
+export interface ProductListRequestAction {
+  type: typeof PRODUCT_LIST_REQUEST;
+  loading: boolean;
+  products: ProductType[];
+}
