ab1478a7edb3ec7311ee599f0cf9e7f387517ef1
RdotSilva <RyandotSilva@gmail.com>
2021-02-10 19:00:39 -0500

    Add addToCart action creator

---
+++ frontend/src/actions/cartActions.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+/**
+ * Add to cart action creator
+ * Actions related to adding products to the cart
+ */
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
