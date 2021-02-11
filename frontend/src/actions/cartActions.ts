3a7cdde49ce78d38a1c963b0f47074dc438ce8c8
RdotSilva <RyandotSilva@gmail.com>
2021-02-10 19:41:10 -0500

    Update types

--- frontend/src/actions/cartActions.ts
+++ frontend/src/actions/cartActions.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
+import { Dispatch } from "react";
 import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { AppAction } from "../types/actions";
 
 /**
  * Add to cart action creator
  * Actions related to adding products to the cart
  */
 
-export const addToCart = (id, qty) => async (dispatch, getState) => {
+export const addToCart = (id: string, qty: number) => async (
+  dispatch: Dispatch<AppAction>,
+  getState: any // TODO change type
+) => {
   const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
