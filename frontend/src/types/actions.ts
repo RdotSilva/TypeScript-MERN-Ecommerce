bd10982ddb50d088212ee5196c679b7b67cf6fd8
RdotSilva <RyandotSilva@gmail.com>
2021-02-10 19:05:54 -0500

    Update action types for cart

--- frontend/src/types/actions.ts
+++ frontend/src/types/actions.ts
@@ -1,3 +1,4 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
 import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
@@ -11,6 +12,10 @@ import { ProductType } from "./../types";
 /**
  * Add an interface for each action type
  * These will be aggregated and used in the AppAction type
+ */
+
+/**
+ * ProductList interfaces
  */
 export interface ProductListRequestAction {
   type: typeof PRODUCT_LIST_REQUEST;
@@ -29,6 +34,10 @@ export interface ProductListFailAction {
   loading?: boolean;
   payload?: Error;
 }
+
+/**
+ * Product Details interfaces
+ */
 
 export interface ProductDetailsRequestAction {
   type: typeof PRODUCT_DETAILS_REQUEST;
@@ -49,7 +58,16 @@ export interface ProductDetailsFailAction {
 }
 
 /**
- * ProductList actions aggregated into one total action type
+ * Cart Add Item interface
+ */
+
+export interface AddToCartAction {
+  type: typeof CART_ADD_ITEM;
+  payload: ProductType;
+}
+
+/**
+ * Above actions aggregated into one total action type
  */
 
 export type ProductListActions =
@@ -62,9 +80,14 @@ export type ProductDetailsActions =
   | ProductDetailsSuccessAction
   | ProductDetailsFailAction;
 
+export type CartActions = AddToCartAction;
+
 /**
  * Aggregate of ALL actions types to use within the entire application
  * Combine all of your Redux action aggregates into this
  */
 
-export type AppAction = ProductListActions | ProductDetailsActions;
+export type AppAction =
+  | ProductListActions
+  | ProductDetailsActions
+  | CartActions;
