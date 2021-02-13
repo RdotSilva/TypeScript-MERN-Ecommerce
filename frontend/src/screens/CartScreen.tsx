4ba27f92da23540825acdc8f16dbe2c576b9953e
RdotSilva <RyandotSilva@gmail.com>
2021-02-12 20:44:35 -0500

    Add Form Control to handle item quantity

--- frontend/src/screens/CartScreen.tsx
+++ frontend/src/screens/CartScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { ChangeEvent, useEffect } from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -54,6 +54,29 @@ const CartScreen = ({ match, location, history }: Props) => {
                 <Row>
                   <Col md={2}>
                     <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={qty}
+                      onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {/* Populate the Qty dropdown selector based on the number of items in stock */}
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {" "}
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
                   </Col>
                 </Row>
               </ListGroup.Item>;
