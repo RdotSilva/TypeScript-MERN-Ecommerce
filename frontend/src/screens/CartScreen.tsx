930e3c953e258b4d4a0725e581d9994f64e69894
RdotSilva <RyandotSilva@gmail.com>
2021-02-12 20:41:07 -0500

    Update cart display to show item image

--- frontend/src/screens/CartScreen.tsx
+++ frontend/src/screens/CartScreen.tsx
@@ -48,7 +48,17 @@ const CartScreen = ({ match, location, history }: Props) => {
             Your cart is empty <Link to="/">Go Back</Link>
           </Message>
         ) : (
-          <ListGroup variant="flush"></ListGroup>
+          <ListGroup variant="flush">
+            {cartItems.map((item) => {
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                </Row>
+              </ListGroup.Item>;
+            })}
+          </ListGroup>
         )}
       </Col>
       <Col md={2}></Col>
