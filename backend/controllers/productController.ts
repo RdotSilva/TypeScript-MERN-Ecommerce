ad3776136db306bae24832f23d04afaeb295b655
RdotSilva <RyandotSilva@gmail.com>
2021-02-14 19:12:01 -0500

    Add getProductById controller method

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -12,3 +12,22 @@ const getProducts = asyncHandler(async (req: Request, res: Response) => {
 
   res.json(products);
 });
+
+/**
+ * Fetch single product
+ * @route GET /api/products/:id
+ * @param id ID of
+ * @access Public
+ */
+const getProductById = asyncHandler(async (req: Request, res: Response) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById };
