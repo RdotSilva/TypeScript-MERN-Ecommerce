3c948e697e03d858a4c88ba2deb530a55d7e74af
RdotSilva <RyandotSilva@gmail.com>
2021-02-15 19:16:16 -0500

    Update logic to authenticate user

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel";
 
@@ -10,7 +10,20 @@ import User from "../models/userModel";
 const authUser = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  // TODO: Enable user authentication
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 export { authUser };
