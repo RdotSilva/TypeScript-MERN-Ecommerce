7e7c63de170542a0da7344850f0594f20c739053
RdotSilva <RyandotSilva@gmail.com>
2021-02-15 19:37:07 -0500

    Fix bug w/ error message not displaying correctly

--- backend/middleware/errorMiddleware.ts
+++ backend/middleware/errorMiddleware.ts
@@ -1,24 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 
-const notFound = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const notFound = (req: Request, res: Response, next: NextFunction) => {
   const error = new Error(`Not found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-// TODO: Debug error handler. Not passing correct message for the error.
 const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const statusCode: number = res.statusCode === 200 ? 500 : res.statusCode;
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
     message: err.message,
