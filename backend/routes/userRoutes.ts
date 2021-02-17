d073f87771f780565343c009fc002ef46a59dcab
RdotSilva <RyandotSilva@gmail.com>
2021-02-16 19:23:17 -0500

    Update route w/ protect middleware

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { authUser, getUserProfile } from "../controllers/userController";
+import { protect } from "../middleware/authMiddleware";
 
 router.route("/login").post(authUser);
-router.route("/profile").get(getUserProfile);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
