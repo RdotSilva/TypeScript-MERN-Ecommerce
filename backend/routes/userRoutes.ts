441e43f55ca6585a1863883732969f8bf6bff02b
RdotSilva <RyandotSilva@gmail.com>
2021-02-16 19:15:03 -0500

    Add getUserProfile route

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController";
+import { authUser, getUserProfile } from "../controllers/userController";
 
 router.route("/login").post(authUser);
+router.route("/profile").get(getUserProfile);
 
 export default router;
