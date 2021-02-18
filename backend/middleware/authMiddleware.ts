275ccf8132a5d98b3884fc84da6bec12de374fe9
RdotSilva <RyandotSilva@gmail.com>
2021-02-17 21:13:13 -0500

    Add asyncHandler

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -1,15 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel";
+import asyncHandler from "express-async-handler";
 
 /**
  * Middleware used to protect routes from unauthorized users
  */
-const protect = async (req: Request, res: Response, next: NextFunction) => {
-  let token;
-  console.log(req.headers.authorization);
-  next();
-  // TODO: Implement auth protect middleware to protect routes
-};
+const protect = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    
+);
 
 export { protect };
