55a5a2b83e7f4d169864591a536e3b16a23ff6ec
RdotSilva <RyandotSilva@gmail.com>
2021-02-17 21:17:48 -0500

    Update protect logic to check for jwt

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 import User from "../models/userModel";
 import asyncHandler from "express-async-handler";
 
@@ -8,7 +8,28 @@ import asyncHandler from "express-async-handler";
  */
 const protect = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    
+    let token;
+
+    const secret: Secret = process.env.JWT_SECRET!;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        const decoded = jwt.verify(token, secret);
+
+        console.log(decoded);
+      } catch (error) {}
+    }
+
+    if (!token) {
+      res.status(401);
+      throw new Error("Not authorized, no token");
+    }
+  }
 );
 
 export { protect };
