feb873e8e59e58db0b52bac693aa1ed7249dfb63
RdotSilva <RyandotSilva@gmail.com>
2021-02-17 21:50:35 -0500

    Add JSDoc comments

--- backend/types/express.ts
+++ backend/types/express.ts
@@ -1,5 +1,8 @@
 import { Request as Req, Response as Res, NextFunction as Next } from "express";
 
+/**
+ * Custom User interface used in combination with Express Request / Response types
+ */
 interface User {
   user?: {
     _id: string;
@@ -9,6 +12,9 @@ interface User {
   };
 }
 
+/**
+ * Combine Express types with customer User interface
+ */
 export type Request = Req & User;
 export type Response = Res & User;
 export type NextFunction = Next;
