225e55a8d0baecc50c72980efaa0fce8c4a8262d
RdotSilva <RyandotSilva@gmail.com>
2021-02-18 20:16:18 -0500

    Add pre method to hash password

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import { Model, Document } from "mongoose";
+import { NextFunction } from "../types/express";
 
 const mongoose = require("mongoose");
 
@@ -41,6 +42,20 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword: string) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+/**
+ * Runs before the model saves and hecks to see if password has been
+ * modified and hashes the password before saving to database
+ * @param enteredPassword The password that a user enters
+ */
+userSchema.pre("save", async function (this: User, next: NextFunction) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User: Model<User> = mongoose.model("User", userSchema);
 
