ea9924cc5a3f649925bf8938a8f5ccb17e635a4e
RdotSilva <RyandotSilva@gmail.com>
2021-02-19 20:06:02 -0500

    Add Form

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
 
 interface Props {}
 
 const LoginScreen = (props: Props) => {
-  return <div>Login Screen</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default LoginScreen;
