a00ba2d647482e1d2db0cd9ac9a8d31fa03328a3
RdotSilva <RyandotSilva@gmail.com>
2021-02-19 19:53:40 -0500

    Add login action

--- frontend/src/actions/userActions.ts
+++ frontend/src/actions/userActions.ts
@@ -1,3 +1,45 @@
-// TODO: Add action for login
+import axios from "axios";
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "./../constants/userConstants";
+
+export const login = (email: string, password: string) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+
+    // Axios config
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    // Save user info to local storage
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // TODO Add action for logout
