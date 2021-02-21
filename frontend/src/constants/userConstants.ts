27687d27082d49bec2508864ce5fafe576f31881
RdotSilva <RyandotSilva@gmail.com>
2021-02-21 18:58:09 -0500

    Add user registration constants

--- frontend/src/constants/userConstants.ts
+++ frontend/src/constants/userConstants.ts
@@ -5,3 +5,10 @@ export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
 export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL";
 export const USER_LOGOUT = "USER_LOGOUT";
+
+/**
+ * User registration constants used for Redux
+ */
+export const USER_REGISTER_REQUEST = "USER_REGISTER_REQUEST";
+export const USER_REGISTER_SUCCESS = "USER_REGISTER_SUCCESS";
+export const USER_REGISTER_FAILURE = "USER_REGISTER_FAILURE";
