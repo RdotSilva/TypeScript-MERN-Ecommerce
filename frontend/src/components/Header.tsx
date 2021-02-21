c23a4a547db1883977a2f15b61c061e94ad33257
RdotSilva <RyandotSilva@gmail.com>
2021-02-21 18:37:14 -0500

    Add userInfo from selector

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 
 interface Props {}
@@ -8,6 +9,9 @@ interface Props {}
  * Header component
  */
 const Header = (props: Props) => {
+  const userLogin = useSelector((state: any) => state.userLogin);
+  const { userInfo } = userLogin;
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -15,18 +19,12 @@ const Header = (props: Props) => {
           <LinkContainer to="/">
             <Navbar.Brand>MERN ECommerce</Navbar.Brand>
           </LinkContainer>
-
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i className="fas fa-user"></i>Sign In
                 </Nav.Link>
               </LinkContainer>
             </Nav>
