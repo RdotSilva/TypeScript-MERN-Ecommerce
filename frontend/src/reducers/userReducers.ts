721820bd58d0a77731e45567892966f600d7de5a
RdotSilva <RyandotSilva@gmail.com>
2021-02-22 19:22:46 -0500

    Add userRegisterReducer

--- frontend/src/reducers/userReducers.ts
+++ frontend/src/reducers/userReducers.ts
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAILURE,
 } from "./../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action: any) => {
@@ -15,6 +18,19 @@ export const userLoginReducer = (state = {}, action: any) => {
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action: any) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAILURE:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
