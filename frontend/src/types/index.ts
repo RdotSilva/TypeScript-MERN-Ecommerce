dccc1d2440f0a4858f251f07e5970ad66e133307
RdotSilva <RyandotSilva@gmail.com>
2021-02-23 19:48:14 -0500

    Comment out type exports until new types are added

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./User";
-export * from "./UserLogin";
-export * from "./UserRegister";
-export * from "./Cart";
-export * from "./Product";
+// TODO: Uncomment types as the new ones are added. This is for reference for now.
+// export * from "./UserLogin";
+// export * from "./UserRegister";
+// export * from "./Cart";
+// export * from "./Product";
