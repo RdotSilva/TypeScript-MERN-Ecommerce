d1fb4e8a2275313e519f2cb99aed570496844ea4
RdotSilva <RyandotSilva@gmail.com>
2021-02-25 21:39:41 -0500

    Update exports

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,6 +1,6 @@
 export * from "./User";
-// TODO: Uncomment types as the new ones are added. This is for reference for now.
 export * from "./UserLogin";
 export * from "./UserRegister";
 export * from "./Cart";
-// export * from "./Product";
+export * from "./Product";
+export * from "./ProductDetails";
