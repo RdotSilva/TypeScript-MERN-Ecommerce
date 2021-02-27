db4ec13a1740fa8f1b03f32bf5f04ade5571aabc
RdotSilva <RyandotSilva@gmail.com>
2021-02-26 19:04:52 -0500

    Refactor listProducts action creator

--- frontend/src/actions/productActions.ts
+++ frontend/src/actions/productActions.ts
@@ -1,28 +1,25 @@
 import { ProductDetailsActionTypes } from "./../types/ProductDetails";
 import axios from "axios";
-import { Dispatch } from "redux";
-import { AppAction } from "../types/actions";
-import {
-  PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-} from "./../constants/productConstants";
 import { AppThunk } from "../store";
+import { ProductListActionTypes } from "../types/ProductList";
 
 /**
  * List Products action creator
  * Actions related to listing all products
  */
-export const listProducts = () => async (dispatch: Dispatch<AppAction>) => {
+export const listProducts = (): AppThunk => async (dispatch) => {
   try {
-    dispatch({ type: PRODUCT_LIST_REQUEST });
+    dispatch({ type: ProductListActionTypes.PRODUCT_LIST_REQUEST });
 
     const { data } = await axios.get("/api/products");
 
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    dispatch({
+      type: ProductListActionTypes.PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
   } catch (error) {
     dispatch({
-      type: PRODUCT_LIST_FAIL,
+      type: ProductListActionTypes.PRODUCT_LIST_FAILURE,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
