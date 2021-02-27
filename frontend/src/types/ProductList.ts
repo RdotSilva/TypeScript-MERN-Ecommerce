84e22d9bc8336a1aac9df6303110e10f1cf82a59
RdotSilva <RyandotSilva@gmail.com>
2021-02-26 19:00:42 -0500

    Add ProductList types

---
+++ frontend/src/types/ProductList.ts
@@ -0,0 +1,34 @@
+import { Product } from "./";
+
+export interface ProductListState {
+  products: Product[];
+  pages?: number;
+  page?: number;
+  loading: boolean;
+  error?: undefined;
+}
+
+export enum ProductListActionTypes {
+  PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST",
+  PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS",
+  PRODUCT_LIST_FAILURE = "PRODUCT_LIST_FAILURE",
+}
+
+export interface FetchProductsRequestAction {
+  type: ProductListActionTypes.PRODUCT_LIST_REQUEST;
+}
+
+export interface FetchProductsSuccessAction {
+  type: ProductListActionTypes.PRODUCT_LIST_SUCCESS;
+  payload: { products: Product[]; pages: number; page: number };
+}
+
+export interface FetchProductsFailureAction {
+  type: ProductListActionTypes.PRODUCT_LIST_FAILURE;
+  payload: any;
+}
+
+export type ProductListAction =
+  | FetchProductsSuccessAction
+  | FetchProductsFailureAction
+  | FetchProductsRequestAction;
