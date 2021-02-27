11d9ec240cff522e3c5d26352134f8faea5c217f
RdotSilva <RyandotSilva@gmail.com>
2021-02-26 19:09:22 -0500

    Update ReduxState interface

--- frontend/src/types/ReduxState.ts
+++ frontend/src/types/ReduxState.ts
@@ -1,7 +1,13 @@
 import { UserRegisterState } from "./";
 import { UserLoginState } from "./";
+import { CartState } from "./";
+import { ProductDetailsState } from "./";
+import { ProductListState } from "./";
 
 export interface ReduxState {
   userLogin: UserLoginState;
   userRegister: UserRegisterState;
+  productList: ProductListState;
+  productDetails: ProductDetailsState;
+  cart: CartState;
 }
