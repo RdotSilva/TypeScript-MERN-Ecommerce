b8e158ee6132dd0a049b1b90a7e7c7679d36d755
RdotSilva <RyandotSilva@gmail.com>
2021-02-28 17:29:58 -0500

    Add updateUserProfile controller

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -87,4 +87,35 @@ const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * Update user profile
+ * @route PUT /api/users/profile
+ * @access Private
+ */
+const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
+  const user = await User.findById(req.user?._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export { authUser, getUserProfile, registerUser };
