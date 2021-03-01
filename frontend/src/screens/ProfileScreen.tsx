fa42453df586b5f4b62d725d4a15b68d70de6dc8
RdotSilva <RyandotSilva@gmail.com>
2021-03-01 18:37:48 -0500

    Add state properties

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { ReduxState } from "../types/ReduxState";
 
 interface Props {}
 
 const ProfileScreen = (props: Props) => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const { user, loading, error } = useSelector(
+    (state: ReduxState) => state.userDetails
+  );
+
   return <div></div>;
 };
 
