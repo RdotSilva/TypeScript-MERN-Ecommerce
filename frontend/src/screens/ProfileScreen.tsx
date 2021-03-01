b711c903761e49bffede100ab7c4ef840891a428
RdotSilva <RyandotSilva@gmail.com>
2021-03-01 18:42:40 -0500

    Add useEffect hook

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RouteComponentProps } from "react-router-dom";
+import { getUserDetails } from "../actions/userActions";
 import { ReduxState } from "../types/ReduxState";
 
-interface Props {}
+interface Props extends RouteComponentProps {}
 
-const ProfileScreen = (props: Props) => {
+const ProfileScreen = ({ history }: Props) => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [message, setMessage] = useState<string>("");
 
+  const dispatch = useDispatch();
+
   const { user, loading, error } = useSelector(
     (state: ReduxState) => state.userDetails
   );
+
+  const { userInfo } = useSelector((state: ReduxState) => state.userLogin);
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/login");
+    } else {
+      if (!user?.name) {
+        dispatch(getUserDetails("profile"));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
+    }
+  }, [dispatch, history, userInfo]);
 
   return <div></div>;
 };
