537b39aaa7dfddbef3dabdf6b487dedcfdf3cd64
RdotSilva <RyandotSilva@gmail.com>
2021-03-02 19:17:46 -0500

    Add action types

--- frontend/src/types/UserUpdateProfile.ts
+++ frontend/src/types/UserUpdateProfile.ts
@@ -13,3 +13,22 @@ export enum UserUpdateProfileActionTypes {
   USER_UPDATE_PROFILE_FAILURE = "USER_UPDATE_PROFILE_FAILURE",
   USER_UPDATE_PROFILE_RESET = "USER_UPDATE_PROFILE_RESET",
 }
+
+export interface UserUpdateProfileRequestAction {
+  type: UserUpdateProfileActionTypes.USER_UPDATE_PROFILE_REQUEST;
+}
+
+export interface UserUpdateProfileSuccessAction {
+  type: UserUpdateProfileActionTypes.USER_UPDATE_PROFILE_SUCCESS;
+  payload: TokenUser;
+}
+
+export interface UserUpdateProfileFailureAction {
+  type: UserUpdateProfileActionTypes.USER_UPDATE_PROFILE_FAILURE;
+  payload: any;
+}
+
+export type UserUpdateProfileAction =
+  | UserUpdateProfileRequestAction
+  | UserUpdateProfileSuccessAction
+  | UserUpdateProfileFailureAction;
