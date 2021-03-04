6e3919fee97d1765cd9835145cc10ddf9531c3ea
RdotSilva <RyandotSilva@gmail.com>
2021-03-03 20:06:36 -0500

    Add state items for address

--- frontend/src/screens/ShippingScreen.tsx
+++ frontend/src/screens/ShippingScreen.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import FormContainer from "../components/FormContainer";
 
 interface Props {}
 
-const ShippingScreen = (props: Props) => {
-  return <div>Shipping Screen</div>;
+const ShippingScreen = ({ history }: Props) => {
+  const [address, setAddress] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+  const [postalCode, setPostalCode] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
+
+  return (
+    <FormContainer>
+      <h1>Shipping</h1>
+    </FormContainer>
+  );
 };
 
 export default ShippingScreen;
