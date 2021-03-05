14345332b509ece9a9bffd9a98bf7f30ce10ac0c
RdotSilva <RyandotSilva@gmail.com>
2021-03-04 19:54:16 -0500

    Update initial state w/ shipping address

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -41,8 +41,15 @@ const userInfoFromLocalStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo")!)
   : null;
 
+const shippingAddressFromLocalStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress")!)
+  : null;
+
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage },
+  cart: {
+    cartItems: cartItemsFromLocalStorage,
+    shippingAddress: shippingAddressFromLocalStorage,
+  },
   userLogin: { userInfo: userInfoFromLocalStorage },
 };
 
