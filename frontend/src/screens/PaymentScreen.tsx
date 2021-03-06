f5ba71238d0c3b23c03957453175a8c51f470e8c
RdotSilva <RyandotSilva@gmail.com>
2021-03-05 19:51:47 -0500

    Add payment method form

--- frontend/src/screens/PaymentScreen.tsx
+++ frontend/src/screens/PaymentScreen.tsx
@@ -1,8 +1,10 @@
 import React, { FormEvent, useState } from "react";
-
+import { Button, Form, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { RouteComponentProps } from "react-router-dom";
-
+import { savePaymentMethod } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
+import FormContainer from "../components/FormContainer";
 import { AppDispatch } from "../store";
 import { ReduxState } from "../types/ReduxState";
 
@@ -14,7 +16,40 @@ const PaymentScreen = ({ history }: Props) => {
 
   const [paymentMethod, setPaymentMethod] = useState<string>("PayPal");
 
-  return <div>Payment Screen</div>;
+  return (
+    <FormContainer>
+      <CheckoutSteps stepOne stepTwo stepThree />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Payment Method</Form.Label>
+        </Form.Group>
+        <Col>
+          <Form.Check
+            type="radio"
+            label="PayPal or Credit Card"
+            id="PayPal"
+            name="paymentMethod"
+            value="PayPal"
+            checked
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          ></Form.Check>
+          <Form.Check
+            type="radio"
+            label="Stripe"
+            id="Stripe"
+            name="paymentMethod"
+            value="Stripe"
+            checked
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          ></Form.Check>
+        </Col>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default PaymentScreen;
