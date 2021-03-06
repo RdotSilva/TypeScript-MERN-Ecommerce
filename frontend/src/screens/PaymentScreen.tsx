ce93ff797e53b4163ecee54eb1751a4c85eebfea
RdotSilva <RyandotSilva@gmail.com>
2021-03-05 19:45:41 -0500

    Add paymentMethod state

--- frontend/src/screens/PaymentScreen.tsx
+++ frontend/src/screens/PaymentScreen.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
-interface Props {}
+import { useDispatch, useSelector } from "react-redux";
+import { RouteComponentProps } from "react-router-dom";
 
-const PaymentScreen = (props: Props) => {
+import { AppDispatch } from "../store";
+import { ReduxState } from "../types/ReduxState";
+
+interface Props extends RouteComponentProps {}
+
+const PaymentScreen = ({ history }: Props) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { shippingAddress } = useSelector((state: ReduxState) => state.cart);
+
+  const [paymentMethod, setPaymentMethod] = useState<string>("PayPal");
+
   return <div>Payment Screen</div>;
 };
 
