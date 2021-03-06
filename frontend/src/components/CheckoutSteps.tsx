56d55f9e8e0a070a2606e8211796fae45ca3b304
RdotSilva <RyandotSilva@gmail.com>
2021-03-05 19:28:48 -0500

    Add checkout Nav Links

--- frontend/src/components/CheckoutSteps.tsx
+++ frontend/src/components/CheckoutSteps.tsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 interface Props {}
 
-const CheckoutSteps = (props: Props) => {
-  return <div>Checkout Steps</div>;
+const CheckoutSteps = ({ stepOne, stepTwo, stepThree, stepFour }: Props) => {
+  return (
+    <Nav className="justify-content-center mb-4">
+      <Nav.Item>
+        {stepOne ? (
+          <LinkContainer to="/login">
+            <Nav.Link>Sign In</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Sign In</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {stepTwo ? (
+          <LinkContainer to="/shipping">
+            <Nav.Link>Shipping</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {stepThree ? (
+          <LinkContainer to="/payment">
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {stepFour ? (
+          <LinkContainer to="/placeorder">
+            <Nav.Link>Place Order</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Place Order</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
 };
 
 export default CheckoutSteps;
