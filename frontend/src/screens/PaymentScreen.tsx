048c0efec17fd75a54dbefe14fc963c86bcac65c
RdotSilva <RyandotSilva@gmail.com>
2021-03-05 19:55:37 -0500

    Add submitHandler method

--- frontend/src/screens/PaymentScreen.tsx
+++ frontend/src/screens/PaymentScreen.tsx
@@ -10,11 +10,29 @@ import { ReduxState } from "../types/ReduxState";
 
 interface Props extends RouteComponentProps {}
 
+/**
+ * Component that contains the form for a user to select a payment method
+ */
 const PaymentScreen = ({ history }: Props) => {
   const dispatch = useDispatch<AppDispatch>();
   const { shippingAddress } = useSelector((state: ReduxState) => state.cart);
 
+  // Bring user back to shipping screen if they haven't provided an address
+  if (!shippingAddress) {
+    history.push("/shipping");
+  }
+
   const [paymentMethod, setPaymentMethod] = useState<string>("PayPal");
+
+  /**
+   * Save users shipping address to payment method and send them to the place order screen
+   * @param e HTML event form element
+   */
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
 
   return (
     <FormContainer>
