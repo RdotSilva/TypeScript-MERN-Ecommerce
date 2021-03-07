8510ad8793842af72577c61ec23f9ff7232a3657
RdotSilva <RyandotSilva@gmail.com>
2021-03-07 16:28:08 -0500

    Add addOrderItems controller

---
+++ backend/controllers/orderController.ts
@@ -0,0 +1,41 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel";
+import { Request, Response } from "../types/express";
+
+/**
+ * Create new order
+ * @route GET /api/orders
+ * @access Private
+ */
+const addOrderItems = asyncHandler(async (req: Request, res: Response) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user?._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
