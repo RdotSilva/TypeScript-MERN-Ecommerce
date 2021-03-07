772edb58d65de4d2485a17b39cbcb4746b5e86b0
RdotSilva <RyandotSilva@gmail.com>
2021-03-06 19:16:47 -0500

    Add display for shipping address

--- frontend/src/screens/PlaceOrderScreen.tsx
+++ frontend/src/screens/PlaceOrderScreen.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Col, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { AppDispatch } from "../store";
+import { ReduxState } from "../types/ReduxState";
 
 interface Props {}
 
 const PlaceOrderScreen = (props: Props) => {
-  return <div></div>;
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { cartItems, paymentMethod, shippingAddress } = useSelector(
+    (state: ReduxState) => state.cart
+  );
+
+  return (
+    <>
+      <CheckoutSteps stepOne stepTwo stepThree stepFour />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address:</strong>
+                {shippingAddress?.address}, {shippingAddress?.city}{" "}
+                {shippingAddress?.postalCode} {shippingAddress?.country}
+              </p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default PlaceOrderScreen;
