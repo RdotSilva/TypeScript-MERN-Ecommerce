511bd961bb05456b2c5e7f7b7695fc64215377b8
RdotSilva <RyandotSilva@gmail.com>
2021-03-08 19:56:50 -0500

    Add action types

--- frontend/src/types/OrderCreate.ts
+++ frontend/src/types/OrderCreate.ts
@@ -1,5 +1,33 @@
-export enum OrderActionTypes {
+import { Order } from "./Order";
+
+export interface OrderCreateState {
+  loading: boolean;
+  order?: Order;
+  success?: boolean;
+  error?: any;
+}
+
+export enum OrderCreateActionTypes {
   ORDER_CREATE_REQUEST = "ORDER_CREATE_REQUEST",
   ORDER_CREATE_SUCCESS = "ORDER_CREATE_SUCCESS",
   ORDER_CREATE_FAILURE = "ORDER_CREATE_FAILURE",
 }
+
+export interface OrderCreateRequestAction {
+  type: OrderCreateActionTypes.ORDER_CREATE_REQUEST;
+}
+
+export interface OrderCreateSuccessAction {
+  type: OrderCreateActionTypes.ORDER_CREATE_SUCCESS;
+  payload: Order;
+}
+
+export interface OrderCreateFailureAction {
+  type: OrderCreateActionTypes.ORDER_CREATE_FAILURE;
+  payload: any;
+}
+
+export type OrderCreateAction =
+  | OrderCreateRequestAction
+  | OrderCreateSuccessAction
+  | OrderCreateFailureAction;
