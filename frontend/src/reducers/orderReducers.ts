2c7a969a99f23de915122f1422853e745cb404d1
RdotSilva <RyandotSilva@gmail.com>
2021-03-10 19:30:32 -0500

    Add orderDetailsReducer

--- frontend/src/reducers/orderReducers.ts
+++ frontend/src/reducers/orderReducers.ts
@@ -3,6 +3,11 @@ import {
   OrderCreateActionTypes,
   OrderCreateState,
 } from "../types/OrderCreate";
+import {
+  OrderDetailsAction,
+  OrderDetailsActionTypes,
+  OrderDetailsState,
+} from "../types/OrderDetails";
 
 const orderCreateInitialState: OrderCreateState = {
   loading: false,
@@ -27,6 +32,38 @@ export const orderCreateReducer = (
         order: action.payload,
       };
     case OrderCreateActionTypes.ORDER_CREATE_FAILURE:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const orderDetailsInitialState: OrderDetailsState = {
+  loading: false,
+};
+
+/**
+ * Reducer used for order details logic
+ */
+export const orderDetailsReducer = (
+  state: OrderDetailsState = orderDetailsInitialState,
+  action: OrderDetailsAction
+) => {
+  switch (action.type) {
+    case OrderDetailsActionTypes.ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case OrderDetailsActionTypes.ORDER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        order: action.payload,
+      };
+    case OrderDetailsActionTypes.ORDER_DETAILS_FAILURE:
       return {
         loading: false,
         error: action.payload,
