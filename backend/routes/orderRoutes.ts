63efafc0c52918bcad0b606275bf0a6c5a442a1b
RdotSilva <RyandotSilva@gmail.com>
2021-03-10 19:10:38 -0500

    Add route for order by id

--- backend/routes/orderRoutes.ts
+++ backend/routes/orderRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderController";
+import { addOrderItems, getOrderById } from "../controllers/orderController";
 import { protect } from "../middleware/authMiddleware";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
