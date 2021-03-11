4d02c5981bd85064c15e59a71a28598f6b1afef5
RdotSilva <RyandotSilva@gmail.com>
2021-03-10 19:27:25 -0500

    Add OrderDetails interface

--- frontend/src/types/OrderDetails.ts
+++ frontend/src/types/OrderDetails.ts
@@ -1,3 +1,20 @@
+import { Order } from "./Order";
+
+export interface OrderDetails extends Order {
+  _id: string;
+  user: {
+    _id: string;
+    name: string;
+    email: string;
+  };
+}
+
+export interface OrderDetailsState {
+  loading: boolean;
+  order?: OrderDetails;
+  error?: any;
+}
+
 export enum OrderDetailsActionTypes {
   ORDER_DETAILS_REQUEST = "ORDER_DETAILS_REQUEST",
   ORDER_DETAILS_SUCCESS = "ORDER_DETAILS_SUCCESS",
