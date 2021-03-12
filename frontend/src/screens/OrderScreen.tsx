cdb38a1aa2698b49906a05b95de03378ac5161fc
RdotSilva <RyandotSilva@gmail.com>
2021-03-11 19:44:59 -0500

    Add display for order items

--- frontend/src/screens/OrderScreen.tsx
+++ frontend/src/screens/OrderScreen.tsx
@@ -3,6 +3,7 @@ import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { getOrderDetails } from "../actions/orderActions";
+import Loader from "../components/Loader.";
 import Message from "../components/Message";
 import { AppDispatch } from "../store";
 import { ReduxState } from "../types/ReduxState";
@@ -13,10 +14,6 @@ const OrderScreen = ({ match }: Props) => {
   const orderId = match.params.id;
 
   const dispatch = useDispatch<AppDispatch>();
-
-  const { cartItems, paymentMethod, shippingAddress } = useSelector(
-    (state: ReduxState) => state.cart
-  );
 
   const { order, loading, error } = useSelector(
     (state: ReduxState) => state.orderDetails
@@ -29,7 +26,100 @@ const OrderScreen = ({ match }: Props) => {
     dispatch(getOrderDetails(orderId));
   }, []);
 
-  return <div>Order Screen</div>;
+  return loading ? (
+    <Loader />
+  ) : error || !order ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address:</strong>
+                {order.shippingAddress?.address}, {order.shippingAddress?.city}{" "}
+                {order.shippingAddress?.postalCode}{" "}
+                {order.shippingAddress?.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method:</strong>
+              {order.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.length === 0 ? (
+                <Message>Your Order is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.orderItems.map((orderItem, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={orderItem.image}
+                            alt={orderItem.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${orderItem.product}`}>
+                            {orderItem.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {orderItem.qty} x {orderItem.price} = $
+                          {orderItem.qty * orderItem.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${order.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${order.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${order.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default OrderScreen;
