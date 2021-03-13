4f256b3536bc7347bd36d31e4e48a9a77e9123b4
RdotSilva <RyandotSilva@gmail.com>
2021-03-13 14:17:34 -0500

    Add OrderPayAction interfaces

--- frontend/src/types/OrderPay.ts
+++ frontend/src/types/OrderPay.ts
@@ -1,6 +1,35 @@
+export interface OrderPayState {
+  loading?: boolean;
+  success?: boolean;
+  error?: any;
+}
+
 export enum OrderPayActionTypes {
   ORDER_PAY_REQUEST = "ORDER_PAY_REQUEST",
   ORDER_PAY_SUCCESS = "ORDER_PAY_SUCCESS",
   ORDER_PAY_FAILURE = "ORDER_PAY_FAILURE",
   ORDER_PAY_RESET = "ORDER_PAY_RESET",
 }
+
+export interface OrderPayRequestAction {
+  type: OrderPayActionTypes.ORDER_PAY_REQUEST;
+}
+
+export interface OrderPaySuccessAction {
+  type: OrderPayActionTypes.ORDER_PAY_SUCCESS;
+}
+
+export interface OrderPayFailureAction {
+  type: OrderPayActionTypes.ORDER_PAY_FAILURE;
+  payload: any;
+}
+
+export interface OrderPayResetAction {
+  type: OrderPayActionTypes.ORDER_PAY_RESET;
+}
+
+export type OrderPayAction =
+  | OrderPayRequestAction
+  | OrderPaySuccessAction
+  | OrderPayFailureAction
+  | OrderPayResetAction;
