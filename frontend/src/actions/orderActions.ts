c13a4093c80b431036d51ee99f02da4f37bcee12
RdotSilva <RyandotSilva@gmail.com>
2021-03-13 14:29:03 -0500

    Add payOrder action

--- frontend/src/actions/orderActions.ts
+++ frontend/src/actions/orderActions.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { AppThunk } from "../store";
-import { OrderDetailsActionTypes, OrderDetails } from "../types/";
+import {
+  OrderDetailsActionTypes,
+  OrderDetails,
+  OrderPayActionTypes,
+} from "../types/";
 import { Order } from "../types/Order";
 import { OrderCreate, OrderCreateActionTypes } from "../types/OrderCreate";
 import { errorHandler } from "./errorHandler";
@@ -81,6 +85,49 @@ export const getOrderDetails = (id: string): AppThunk => async (
   } catch (error) {
     dispatch({
       type: OrderDetailsActionTypes.ORDER_DETAILS_FAILURE,
+      payload: errorHandler(error),
+    });
+  }
+};
+
+/**
+ * Action used to pay an order
+ */
+export const payOrder = (
+  orderId: string,
+  paymentResult: any
+): AppThunk => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: OrderPayActionTypes.ORDER_PAY_REQUEST,
+    });
+
+    // Get user info from the userLogin object (from getState)
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // Axios config
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo?.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/orders/${orderId}/pay`,
+      paymentResult,
+      config
+    );
+
+    dispatch({
+      type: OrderPayActionTypes.ORDER_PAY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: OrderPayActionTypes.ORDER_PAY_FAILURE,
       payload: errorHandler(error),
     });
   }
