13130a086eb3c9f7e7eb272897adf9bd4d6a4f2b
RdotSilva <RyandotSilva@gmail.com>
2021-03-13 14:13:11 -0500

    Add route for order payment

--- backend/routes/orderRoutes.ts
+++ backend/routes/orderRoutes.ts
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/orderController";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController";
 import { protect } from "../middleware/authMiddleware";
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
