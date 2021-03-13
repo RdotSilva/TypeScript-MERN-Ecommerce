1988125778881278d23ddeb264a6c4049d09504a
RdotSilva <RyandotSilva@gmail.com>
2021-03-12 20:36:23 -0500

    Add user name/email to order display

--- frontend/src/screens/OrderScreen.tsx
+++ frontend/src/screens/OrderScreen.tsx
@@ -45,6 +45,13 @@ const OrderScreen = ({ match }: Props) => {
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
+                <strong>Name: </strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong>
+                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+              </p>
+              <p>
                 <strong>Address:</strong>
                 {order.shippingAddress?.address}, {order.shippingAddress?.city}{" "}
                 {order.shippingAddress?.postalCode}{" "}
