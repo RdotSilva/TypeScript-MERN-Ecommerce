02ace7ccb277a05599e0578e2c5088952120f467
RdotSilva <RyandotSilva@gmail.com>
2021-03-13 14:12:00 -0500

    Finish TODO for updateOrderToPaid implementation

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -68,7 +68,18 @@ const updateOrderToPaid = asyncHandler(async (req: Request, res: Response) => {
   const order = await Order.findById(req.params.id);
 
   if (order) {
-    //TODO: Finish implementation
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    // Payment result comes from PayPal API
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
   } else {
     res.status(404);
     throw new Error("Order not found");
