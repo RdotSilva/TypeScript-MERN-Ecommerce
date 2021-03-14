a6bd5917d9c210f2978fad0fe42799b9281631f9
RdotSilva <RyandotSilva@gmail.com>
2021-03-14 16:50:35 -0400

    Fix export

--- backend/controllers/paypalController.ts
+++ backend/controllers/paypalController.ts
@@ -8,3 +8,5 @@ import { Request, Response } from "../types/express";
 const fetchPaypalClientId = (req: Request, res: Response) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 };
+
+export { fetchPaypalClientId };
