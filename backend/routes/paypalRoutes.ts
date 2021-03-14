40519d67adda9e204f748a1742e00f2cd0d3a682
RdotSilva <RyandotSilva@gmail.com>
2021-03-14 16:57:40 -0400

    Remove protect middleware

--- backend/routes/paypalRoutes.ts
+++ backend/routes/paypalRoutes.ts
@@ -2,8 +2,6 @@ import express from "express";
 import { fetchPaypalClientId } from "../controllers/paypalController";
 const router = express.Router();
 
-import { protect } from "../middleware/authMiddleware";
-
-router.route("/").get(protect, fetchPaypalClientId);
+router.route("/").get(fetchPaypalClientId);
 
 export default router;
