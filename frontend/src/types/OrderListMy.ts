12b42298ee16bc1d7b16464461d67ccb05d5dcc4
RdotSilva <RyandotSilva@gmail.com>
2021-03-17 19:33:13 -0400

    Add OrderListMyState interface

--- frontend/src/types/OrderListMy.ts
+++ frontend/src/types/OrderListMy.ts
@@ -1,3 +1,22 @@
+import { PaymentResult, Order } from "./";
+
+export interface OrderListMy extends Order {
+  _id: string;
+  user: string;
+  isPaid: boolean;
+  isDelivered: boolean;
+  paidAt?: string;
+  deliveredAt?: string;
+  createdAt: string;
+  paymentResult?: PaymentResult;
+}
+
+export interface OrderListMyState {
+  orders: OrderListMy[];
+  loading: boolean;
+  error?: any;
+}
+
 export enum OrderListMyActionTypes {
   ORDER_LIST_MY_REQUEST = "ORDER_LIST_MY_REQUEST",
   ORDER_LIST_MY_SUCCESS = "ORDER_LIST_MY_SUCCESS",
