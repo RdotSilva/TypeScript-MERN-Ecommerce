c3c68e1066972017747eb35ed113c930982a4549
RdotSilva <RyandotSilva@gmail.com>
2021-03-17 19:36:32 -0400

    Add OrderListMyAction type

--- frontend/src/types/OrderListMy.ts
+++ frontend/src/types/OrderListMy.ts
@@ -23,3 +23,27 @@ export enum OrderListMyActionTypes {
   ORDER_LIST_MY_FAILURE = "ORDER_LIST_MY_FAILURE",
   ORDER_LIST_MY_RESET = "ORDER_LIST_MY_RESET",
 }
+
+export interface OrderListMyRequestAction {
+  type: OrderListMyActionTypes.ORDER_LIST_MY_REQUEST;
+}
+
+export interface OrderListMySuccessAction {
+  type: OrderListMyActionTypes.ORDER_LIST_MY_SUCCESS;
+  payload: OrderListMy[];
+}
+
+export interface OrderListMyFailureAction {
+  type: OrderListMyActionTypes.ORDER_LIST_MY_FAILURE;
+  payload: any;
+}
+
+export interface OrderListMyResetAction {
+  type: OrderListMyActionTypes.ORDER_LIST_MY_RESET;
+}
+
+export type OrderListMyAction =
+  | OrderListMyRequestAction
+  | OrderListMySuccessAction
+  | OrderListMyFailureAction
+  | OrderListMyResetAction;
