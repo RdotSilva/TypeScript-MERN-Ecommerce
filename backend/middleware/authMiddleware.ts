5b440b78fff6e46b69b313c46c0617a3dd79ec50
RdotSilva <RyandotSilva@gmail.com>
2021-03-19 19:03:06 -0400

    Add admin middleware method

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -43,4 +43,13 @@ const protect = asyncHandler(
   }
 );
 
-export { protect };
+const admin = (req: Request, res: Response, next: NextFunction) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export { protect, admin };
