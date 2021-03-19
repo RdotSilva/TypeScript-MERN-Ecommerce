154ae099a10b8c93d837568b7cd706cc9ec8ba85
RdotSilva <RyandotSilva@gmail.com>
2021-03-18 20:05:28 -0400

    Add display for my orders

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useEffect, useState } from "react";
 import { Button, Col, Form, Row, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
 import { RouteComponentProps } from "react-router-dom";
 import { listMyOrders } from "../actions/orderActions";
 import { getUserDetails, updateUserProfile } from "../actions/userActions";
@@ -129,7 +130,46 @@ const ProfileScreen = ({ history }: Props) => {
           <Message variant="danger">{errorOrders}</Message>
         ) : (
           <Table striped bordered hover responsive className="table-sm">
-           
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders?.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt?.substring(0, 10)
+                    ) : (
+                      <i className="fas fa-times" style={{ color: "red" }}></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt?.substring(0, 10)
+                    ) : (
+                      <i className="fas fa-times" style={{ color: "red" }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className="btn-sm" variant="light">
+                        Details
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </Table>
         )}
       </Col>
