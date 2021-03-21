051575e4d73c08ffe13e025208afe93bd5ef085d
RdotSilva <RyandotSilva@gmail.com>
2021-03-21 17:41:56 -0400

    Add TSDoc comments

--- frontend/src/actions/errorHandler.ts
+++ frontend/src/actions/errorHandler.ts
@@ -1,3 +1,6 @@
+/**
+ * General error handling method that returns an error message
+ */
 export const errorHandler = (error: any) => {
   return error.response && error.response.data.message
     ? error.response.data.message
