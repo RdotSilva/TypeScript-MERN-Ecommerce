c655f526b4ac523ea4286d3af7d9d1085d63ee4d
RdotSilva <RyandotSilva@gmail.com>
2021-03-21 17:35:48 -0400

    Add deleteUser controller

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -128,4 +128,28 @@ const getUsers = asyncHandler(async (req: Request, res: Response) => {
   res.json(users);
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
+/**
+ * Delete a user
+ * @route DELETE /api/users/:id
+ * @access Private/Admin
+ */
+const deleteUser = asyncHandler(async (req: Request, res: Response) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove;
+    res.json({ message: "User removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
