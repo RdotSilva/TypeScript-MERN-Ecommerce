01bafa635b5cfe0b8a065cb28333b99dcbd9a2cd
RdotSilva <RyandotSilva@gmail.com>
2021-03-20 20:41:03 -0400

    Add useEffect hook

--- frontend/src/screens/UserListScreen.tsx
+++ frontend/src/screens/UserListScreen.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listUsers } from "../actions/userActions";
+import { ReduxState } from "../types/ReduxState";
 
 interface Props {}
 
 const UserListScreen = (props: Props) => {
+  const dispatch = useDispatch();
+
+  const { loading, error, users } = useSelector(
+    (state: ReduxState) => state.userList
+  );
+
+  useEffect(() => {
+    dispatch(listUsers());
+  }, [dispatch]);
+
   return <div></div>;
 };
 
