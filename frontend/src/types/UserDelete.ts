e5311bb08063fc66e7ca9e673fa77693b6d34c17
RdotSilva <RyandotSilva@gmail.com>
2021-03-22 19:39:14 -0400

    Add UserDeleteAction type

--- frontend/src/types/UserDelete.ts
+++ frontend/src/types/UserDelete.ts
@@ -9,3 +9,21 @@ export enum UserDeleteActionTypes {
   USER_DELETE_SUCCESS = "USER_DELETE_SUCCESS",
   USER_DELETE_FAILURE = "USER_DELETE_FAILURE",
 }
+
+export interface UserDeleteRequestAction {
+  type: UserDeleteActionTypes.USER_DELETE_REQUEST;
+}
+
+export interface UserDeleteSuccessAction {
+  type: UserDeleteActionTypes.USER_DELETE_SUCCESS;
+}
+
+export interface UserDeleteFailureAction {
+  type: UserDeleteActionTypes.USER_DELETE_FAILURE;
+  payload: any;
+}
+
+export type UserDeleteAction =
+  | UserDeleteRequestAction
+  | UserDeleteSuccessAction
+  | UserDeleteFailureAction;
