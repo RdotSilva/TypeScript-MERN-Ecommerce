a54ead67cfa2b706644fd28a1856b5c7f7d2250b
RdotSilva <RyandotSilva@gmail.com>
2021-03-23 20:01:18 -0400

    Add new user routes

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -7,6 +7,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController";
 import { admin, protect } from "../middleware/authMiddleware";
 
@@ -16,6 +18,10 @@ router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route("/:id").delete(protect, admin, deleteUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
