38dd22e7f1f2f0a9cd1d892a01b29d209b4f0dac
RdotSilva <RyandotSilva@gmail.com>
2021-03-24 18:44:32 -0400

    Add state for user details

--- frontend/src/screens/UserEditScreen.tsx
+++ frontend/src/screens/UserEditScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router";
 
 interface MatchParams {
@@ -8,6 +8,12 @@ interface MatchParams {
 interface Props extends RouteComponentProps<MatchParams> {}
 
 const UserEditScreen = ({ match, history }: Props) => {
+  const userId = match.params.id;
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+
   return <div>User Edit Screen</div>;
 };
 
