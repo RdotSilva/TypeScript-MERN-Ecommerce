842ccfaecf9e457df9b0f782564f933c463b992b
RdotSilva <RyandotSilva@gmail.com>
2021-03-24 18:46:14 -0400

    Add submitHandler placeholder method

--- frontend/src/screens/UserEditScreen.tsx
+++ frontend/src/screens/UserEditScreen.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
+import { useSelector } from "react-redux";
 import { RouteComponentProps } from "react-router";
+import { ReduxState } from "../types/ReduxState";
 
 interface MatchParams {
   id: string;
@@ -13,6 +15,15 @@ const UserEditScreen = ({ match, history }: Props) => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
+
+  const { user, loading, error } = useSelector(
+    (state: ReduxState) => state.userDetails
+  );
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // TODO: Add logic to dispatch update user action
+  };
 
   return <div>User Edit Screen</div>;
 };
