52cfa81c43bc4d8a3fe149654f560284783c91ed
RdotSilva <RyandotSilva@gmail.com>
2021-03-25 19:25:03 -0400

    Add UserUpdateAction type

--- frontend/src/types/UserUpdate.ts
+++ frontend/src/types/UserUpdate.ts
@@ -4,3 +4,26 @@ export enum UserUpdateActionTypes {
   USER_UPDATE_FAILURE = "USER_UPDATE_FAILURE",
   USER_UPDATE_RESET = "USER_UPDATE_RESET",
 }
+
+export interface UserUpdateRequestAction {
+  type: UserUpdateActionTypes.USER_UPDATE_REQUEST;
+}
+
+export interface UserUpdateSuccessAction {
+  type: UserUpdateActionTypes.USER_UPDATE_SUCCESS;
+}
+
+export interface UserUpdateFailureAction {
+  type: UserUpdateActionTypes.USER_UPDATE_FAILURE;
+  payload: any;
+}
+
+export interface UserUpdateResetAction {
+  type: UserUpdateActionTypes.USER_UPDATE_RESET;
+}
+
+export type UserUpdateAction =
+  | UserUpdateRequestAction
+  | UserUpdateSuccessAction
+  | UserUpdateFailureAction
+  | UserUpdateResetAction;
