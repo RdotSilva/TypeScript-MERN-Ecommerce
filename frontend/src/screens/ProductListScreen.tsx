bb6b28c3a13acc52898473bb89511fcc47db7812
RdotSilva <RyandotSilva@gmail.com>
2021-03-26 19:55:23 -0400

    Add Redux state values

--- frontend/src/screens/ProductListScreen.tsx
+++ frontend/src/screens/ProductListScreen.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../store";
+import { ReduxState } from "../types/ReduxState";
 
 interface Props {}
 
 const ProductListScreen = (props: Props) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { loading, products, error } = useSelector(
+    (state: ReduxState) => state.productList
+  );
+
+  const { userInfo } = useSelector((state: ReduxState) => state.userLogin);
+
   return <div>Product List Screen</div>;
 };
 
