bd241afd875c9b68eab2c5bfec9a0a2078da9657
RdotSilva <RyandotSilva@gmail.com>
2021-03-26 19:57:20 -0400

    Add useEffect hook

--- frontend/src/screens/ProductListScreen.tsx
+++ frontend/src/screens/ProductListScreen.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { RouteComponentProps } from "react-router";
+import { listProducts } from "../actions/productActions";
 import { AppDispatch } from "../store";
 import { ReduxState } from "../types/ReduxState";
 
-interface Props {}
+interface Props extends RouteComponentProps {}
 
-const ProductListScreen = (props: Props) => {
+const ProductListScreen = ({ history }: Props) => {
   const dispatch = useDispatch<AppDispatch>();
 
   const { loading, products, error } = useSelector(
@@ -13,6 +15,17 @@ const ProductListScreen = (props: Props) => {
   );
 
   const { userInfo } = useSelector((state: ReduxState) => state.userLogin);
+
+  /**
+   * If user is Admin load products or redirect to login
+   */
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo]);
 
   return <div>Product List Screen</div>;
 };
