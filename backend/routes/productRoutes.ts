1824064c3a35e4c3b84e6c302dd5e919eed5edae
RdotSilva <RyandotSilva@gmail.com>
2021-03-27 19:04:49 -0400

    Add deleteProduct route

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById } from "../controllers/productController";
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+} from "../controllers/productController";
+import { admin, protect } from "../middleware/authMiddleware";
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
