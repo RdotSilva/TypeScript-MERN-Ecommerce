7da72db202c92a01a7581432b556d09884deb756
RdotSilva <RyandotSilva@gmail.com>
2021-03-30 18:23:42 -0400

    Add ProductCreateState interface

--- frontend/src/types/ProductCreate.ts
+++ frontend/src/types/ProductCreate.ts
@@ -1,3 +1,12 @@
+import { Product } from "./Product";
+
+export interface ProductCreateState {
+  success?: boolean;
+  product?: Product;
+  loading?: boolean;
+  error?: any;
+}
+
 export enum ProductCreateActionTypes {
   PRODUCT_CREATE_REQUEST = "PRODUCT_CREATE_REQUEST",
   PRODUCT_CREATE_SUCCESS = "PRODUCT_CREATE_SUCCESS",
