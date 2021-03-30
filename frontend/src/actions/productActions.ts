b3b9b172fa98a5a8725304594ac00c9fee5f9364
RdotSilva <RyandotSilva@gmail.com>
2021-03-30 18:45:52 -0400

    Add createProduct action

--- frontend/src/actions/productActions.ts
+++ frontend/src/actions/productActions.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AppThunk } from "../store";
 import { ProductListActionTypes } from "../types/ProductList";
 import { errorHandler } from "./errorHandler";
-import { ProductDeleteActionTypes } from "../types/";
+import { ProductCreateActionTypes, ProductDeleteActionTypes } from "../types/";
 
 /**
  * List Products action creator
@@ -83,6 +83,41 @@ export const deleteProduct = (id: string): AppThunk => async (
   } catch (error) {
     dispatch({
       type: ProductDeleteActionTypes.PRODUCT_DELETE_FAILURE,
+      payload: errorHandler(error),
+    });
+  }
+};
+
+/**
+ * Action used to create a product
+ */
+export const createProduct = (): AppThunk => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ProductCreateActionTypes.PRODUCT_CREATE_REQUEST,
+    });
+
+    // Get user info from the userLogin object (from getState)
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // Axios config
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo?.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/products`, {}, config);
+
+    dispatch({
+      type: ProductCreateActionTypes.PRODUCT_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ProductCreateActionTypes.PRODUCT_CREATE_FAILURE,
       payload: errorHandler(error),
     });
   }
