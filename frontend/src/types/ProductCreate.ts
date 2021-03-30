eff355e522db6b362fb93da04caead32f225bae1
RdotSilva <RyandotSilva@gmail.com>
2021-03-30 18:24:55 -0400

    Add ProductCreateAction type

--- frontend/src/types/ProductCreate.ts
+++ frontend/src/types/ProductCreate.ts
@@ -13,3 +13,27 @@ export enum ProductCreateActionTypes {
   PRODUCT_CREATE_FAILURE = "PRODUCT_CREATE_FAILURE",
   PRODUCT_CREATE_RESET = "PRODUCT_CREATE_RESET",
 }
+
+export interface ProductCreateRequestAction {
+  type: ProductCreateActionTypes.PRODUCT_CREATE_REQUEST;
+}
+
+export interface ProductCreateSuccessAction {
+  type: ProductCreateActionTypes.PRODUCT_CREATE_SUCCESS;
+  payload: Product;
+}
+
+export interface ProductCreateFailureAction {
+  type: ProductCreateActionTypes.PRODUCT_CREATE_FAILURE;
+  payload: any;
+}
+
+export interface ProductCreateResetAction {
+  type: ProductCreateActionTypes.PRODUCT_CREATE_RESET;
+}
+
+export type ProductCreateAction =
+  | ProductCreateRequestAction
+  | ProductCreateSuccessAction
+  | ProductCreateFailureAction
+  | ProductCreateResetAction;
