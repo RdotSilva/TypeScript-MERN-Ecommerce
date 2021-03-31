24ffe7ac22ae683182ae59a40d7955e54aaab4f5
RdotSilva <RyandotSilva@gmail.com>
2021-03-31 18:41:41 -0400

    Add product state values

--- frontend/src/screens/ProductEditScreen.tsx
+++ frontend/src/screens/ProductEditScreen.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { RouteComponentProps } from "react-router";
 
-interface Props {}
+interface MatchParams {
+  id: string;
+}
+interface Props extends RouteComponentProps<MatchParams> {}
 
-const ProductEditScreen = (props: Props) => {
+const ProductEditScreen = ({ match }: Props) => {
+  const productId = match.params.id;
+  const [name, setName] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+  const [image, setImage] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [brand, setBrand] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [countInStock, setCountInStock] = useState<number>(0);
+
   return <div>Edit Product Screen</div>;
 };
 
