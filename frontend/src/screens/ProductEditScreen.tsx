c952c935d9e8001e496064eefbe33d956eb538d4
RdotSilva <RyandotSilva@gmail.com>
2021-03-31 18:46:57 -0400

    Add useEffect hook

--- frontend/src/screens/ProductEditScreen.tsx
+++ frontend/src/screens/ProductEditScreen.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RouteComponentProps } from "react-router";
+import { listProductDetails } from "../actions/productActions";
 import { ReduxState } from "../types/ReduxState";
 
 interface MatchParams {
@@ -23,6 +24,23 @@ const ProductEditScreen = ({ match }: Props) => {
   const { product, loading, error } = useSelector(
     (state: ReduxState) => state.productDetails
   );
+
+  /**
+   * Load a product if not currently loaded and email product state values
+   */
+  useEffect(() => {
+    if (!product || product._id !== productId)
+      dispatch(listProductDetails(productId));
+    else {
+      setName(product.name);
+      setPrice(product.price);
+      setImage(product.image);
+      setDescription(product.description);
+      setBrand(product.brand);
+      setCategory(product.category);
+      setCountInStock(product.countInStock);
+    }
+  }, [product, productId, dispatch, history]);
 
   return <div>Edit Product Screen</div>;
 };
