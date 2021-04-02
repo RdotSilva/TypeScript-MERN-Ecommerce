28c069b7bf1f30559ea00ab12171ada18bca91dd
RdotSilva <RyandotSilva@gmail.com>
2021-04-02 19:48:00 -0400

    Add ProductUpdateAction type

--- frontend/src/types/ProductUpdate.ts
+++ frontend/src/types/ProductUpdate.ts
@@ -13,3 +13,27 @@ export enum ProductUpdateActionTypes {
   PRODUCT_UPDATE_FAILURE = "PRODUCT_UPDATE_FAILURE",
   PRODUCT_UPDATE_RESET = "PRODUCT_UPDATE_RESET",
 }
+
+export interface ProductUpdateRequestAction {
+  type: ProductUpdateActionTypes.PRODUCT_UPDATE_REQUEST;
+}
+
+export interface ProductUpdateSuccessAction {
+  type: ProductUpdateActionTypes.PRODUCT_UPDATE_SUCCESS;
+  payload: Product;
+}
+
+export interface ProductUpdateFailureAction {
+  type: ProductUpdateActionTypes.PRODUCT_UPDATE_FAILURE;
+  payload: any;
+}
+
+export interface ProductUpdateResetAction {
+  type: ProductUpdateActionTypes.PRODUCT_UPDATE_RESET;
+}
+
+export type ProductUpdateAction =
+  | ProductUpdateRequestAction
+  | ProductUpdateSuccessAction
+  | ProductUpdateFailureAction
+  | ProductUpdateResetAction;
