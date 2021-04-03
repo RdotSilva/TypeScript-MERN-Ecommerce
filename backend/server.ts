01bb5299e1ab13792bb165c13eb1c32b88b2ea92
RdotSilva <RyandotSilva@gmail.com>
2021-04-03 18:29:26 -0400

    Mount uploadRoutes in server

--- backend/server.ts
+++ backend/server.ts
@@ -8,6 +8,7 @@ import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import paypalRoutes from "./routes/paypalRoutes";
+import uploadRoutes from "./routes/uploadRoutes";
 
 const PORT = process.env.PORT || 5000;
 
@@ -28,6 +29,7 @@ app.use("/api/products/", productRoutes);
 app.use("/api/users/", userRoutes);
 app.use("/api/orders/", orderRoutes);
 app.use("/api/config/paypal", paypalRoutes);
+app.use("/api/upload", uploadRoutes);
 
 // Use Middleware
 app.use(notFound);
