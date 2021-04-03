0a784c7bd37ac73f6f8f6232c9cc5829900d0488
RdotSilva <RyandotSilva@gmail.com>
2021-04-03 18:26:50 -0400

    Add checkFileType method

--- backend/routes/uploadRoutes.ts
+++ backend/routes/uploadRoutes.ts
@@ -1,6 +1,39 @@
 import express from "express";
-import multer from "multer";
+import multer, { Multer } from "multer";
+import path from "path";
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+/**
+ * Check if a file type matches one of the expected extensions (images only)
+ * @param file
+ * @param cb
+ */
+function checkFileType(
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(Error("Images only!"));
+  }
+}
 
 export default router;
