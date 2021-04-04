f41ccea35dbe6633b3bc0b47802948dd25e8bf71
RdotSilva <RyandotSilva@gmail.com>
2021-04-04 19:25:38 -0400

    Add OrderListState interface

--- frontend/src/types/OrderList.ts
+++ frontend/src/types/OrderList.ts
@@ -1,3 +1,26 @@
+import { PaymentResult } from "./";
+import { Order } from "./Order";
+
+export interface OrderList extends Order {
+  _id: string;
+  user: {
+    _id: string;
+    name: string;
+  };
+  isPaid: boolean;
+  isDelivered: boolean;
+  paidAt?: string;
+  deliveredAt?: string;
+  createdAt: string;
+  paymentResult?: PaymentResult;
+}
+
+export interface OrderListState {
+  orders: OrderList[];
+  loading: boolean;
+  error?: any;
+}
+
 export enum OrderListActionTypes {
   ORDER_LIST_REQUEST = "ORDER_LIST_REQUEST",
   ORDER_LIST_SUCCESS = "ORDER_LIST_SUCCESS",
