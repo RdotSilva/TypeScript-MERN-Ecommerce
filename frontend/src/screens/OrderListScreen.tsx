3f2466ce737b47ec9837f276e746188a1ba970fa
RdotSilva <RyandotSilva@gmail.com>
2021-04-05 18:38:26 -0400

    Add useEffect hook

--- frontend/src/screens/OrderListScreen.tsx
+++ frontend/src/screens/OrderListScreen.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RouteComponentProps } from "react-router";
+import { listOrders } from "../actions/orderActions";
 import { ReduxState } from "../types/ReduxState";
 
 interface Props extends RouteComponentProps {}
@@ -11,6 +13,17 @@ const OrderListScreen = ({ history }: Props) => {
     (state: ReduxState) => state.orderList
   );
   const { userInfo } = useSelector((state: ReduxState) => state.userLogin);
+
+  /**
+   * List all orders if user is admin, otherwise bring user to login screen
+   */
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listOrders());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo]);
 
   return <>Order List Screen</>;
 };
