ff64189d62f5a1b4f1dce881674d516ca6bc3c1d
RdotSilva <RyandotSilva@gmail.com>
2021-04-05 18:36:23 -0400

    Add Redux state values

--- frontend/src/screens/OrderListScreen.tsx
+++ frontend/src/screens/OrderListScreen.tsx
@@ -1,10 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RouteComponentProps } from "react-router";
+import { ReduxState } from "../types/ReduxState";
 
 interface Props extends RouteComponentProps {}
 
 const OrderListScreen = ({ history }: Props) => {
   const dispatch = useDispatch();
+
+  const { loading, orders, error } = useSelector(
+    (state: ReduxState) => state.orderList
+  );
+  const { userInfo } = useSelector((state: ReduxState) => state.userLogin);
 
   return <>Order List Screen</>;
 };
