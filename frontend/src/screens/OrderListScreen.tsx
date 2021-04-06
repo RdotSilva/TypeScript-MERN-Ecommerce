33bd2d4940535522aaac7c6309412b33103d0f25
RdotSilva <RyandotSilva@gmail.com>
2021-04-06 19:17:44 -0400

    Add orderListDisplay method

--- frontend/src/screens/OrderListScreen.tsx
+++ frontend/src/screens/OrderListScreen.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
+import { Button, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { RouteComponentProps } from "react-router";
+import { LinkContainer } from "react-router-bootstrap";
 import { listOrders } from "../actions/orderActions";
+import Loader from "../components/Loader.";
+import Message from "../components/Message";
 import { ReduxState } from "../types/ReduxState";
 
 interface Props extends RouteComponentProps {}
@@ -12,6 +16,7 @@ const OrderListScreen = ({ history }: Props) => {
   const { loading, orders, error } = useSelector(
     (state: ReduxState) => state.orderList
   );
+
   const { userInfo } = useSelector((state: ReduxState) => state.userLogin);
 
   /**
@@ -25,7 +30,67 @@ const OrderListScreen = ({ history }: Props) => {
     }
   }, [dispatch, history, userInfo]);
 
-  return <>Order List Screen</>;
+  const orderListDisplay = () => {
+    if (loading) {
+      return <Loader />;
+    } else if (error) {
+      return <Message variant="danger">{error}</Message>;
+    } else {
+      return (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>PRICE</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>${order.totalPrice}</td>
+                <td>
+                  {order.isPaid ? (
+                    order.paidAt?.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  {order.isDelivered ? (
+                    order.deliveredAt?.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      );
+    }
+  };
+
+  return (
+    <>
+      <h1>Orders</h1>
+      {orderListDisplay()}
+    </>
+  );
 };
 
 export default OrderListScreen;
