aeb623c457544685f6a941b6edd3852f678f9835
RdotSilva <RyandotSilva@gmail.com>
2021-04-06 19:25:30 -0400

    Add updateOrderToDelivered controller

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -87,6 +87,28 @@ const updateOrderToPaid = asyncHandler(async (req: Request, res: Response) => {
 });
 
 /**
+ * Update order to delivered
+ * @route GET /api/orders/:id/deliver
+ * @access Private/Admin
+ */
+const updateOrderToDelivered = asyncHandler(
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  }
+);
+
+/**
  * Get all orders from a logged in user
  * @route GET /api/orders/myorders
  * @access Private
@@ -124,6 +146,7 @@ export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders,
 };
