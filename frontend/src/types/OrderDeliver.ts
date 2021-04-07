9083cc7197826a547583633dbd7802bf5e6bed9f
RdotSilva <RyandotSilva@gmail.com>
2021-04-07 19:02:04 -0400

    Add OrderDeliverAction type

--- frontend/src/types/OrderDeliver.ts
+++ frontend/src/types/OrderDeliver.ts
@@ -10,3 +10,26 @@ export enum OrderDeliverActionTypes {
   ORDER_DELIVER_FAILURE = "ORDER_DELIVER_FAILURE",
   ORDER_DELIVER_RESET = "ORDER_DELIVER_RESET",
 }
+
+export interface OrderDeliverRequestAction {
+  type: OrderDeliverActionTypes.ORDER_DELIVER_REQUEST;
+}
+
+export interface OrderDeliverSuccessAction {
+  type: OrderDeliverActionTypes.ORDER_DELIVER_SUCCESS;
+}
+
+export interface OrderDeliverFailureAction {
+  type: OrderDeliverActionTypes.ORDER_DELIVER_FAILURE;
+  payload: any;
+}
+
+export interface OrderDeliverResetAction {
+  type: OrderDeliverActionTypes.ORDER_DELIVER_RESET;
+}
+
+export type OrderDeliverAction =
+  | OrderDeliverRequestAction
+  | OrderDeliverSuccessAction
+  | OrderDeliverFailureAction
+  | OrderDeliverResetAction;
