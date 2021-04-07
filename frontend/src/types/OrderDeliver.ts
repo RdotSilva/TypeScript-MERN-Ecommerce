3c398c57cef83a06c168e19abacdce794bb75e32
RdotSilva <RyandotSilva@gmail.com>
2021-04-07 18:59:19 -0400

    Add OrderDeliverState interface

--- frontend/src/types/OrderDeliver.ts
+++ frontend/src/types/OrderDeliver.ts
@@ -1,3 +1,9 @@
+export interface OrderDeliverState {
+  loading?: boolean;
+  success?: boolean;
+  error?: any;
+}
+
 export enum OrderDeliverActionTypes {
   ORDER_DELIVER_REQUEST = "ORDER_DELIVER_REQUEST",
   ORDER_DELIVER_SUCCESS = "ORDER_DELIVER_SUCCESS",
