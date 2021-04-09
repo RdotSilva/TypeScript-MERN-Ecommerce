5d5694ef214111c2d44946b7cde815c77685e5b2
RdotSilva <RyandotSilva@gmail.com>
2021-04-09 18:43:24 -0400

    Add ProductCreateReviewAction type

--- frontend/src/types/ProductCreateReview.ts
+++ frontend/src/types/ProductCreateReview.ts
@@ -4,3 +4,26 @@ export enum ProductCreateReviewActionTypes {
   PRODUCT_CREATE_REVIEW_FAILURE = "PRODUCT_CREATE_REVIEW_FAILURE",
   PRODUCT_CREATE_REVIEW_RESET = "PRODUCT_CREATE_REVIEW_RESET",
 }
+
+export interface ProductCreateReviewRequestAction {
+  type: ProductCreateReviewActionTypes.PRODUCT_CREATE_REVIEW_REQUEST;
+}
+
+export interface ProductCreateReviewSuccessAction {
+  type: ProductCreateReviewActionTypes.PRODUCT_CREATE_REVIEW_SUCCESS;
+}
+
+export interface ProductCreateReviewFailureAction {
+  type: ProductCreateReviewActionTypes.PRODUCT_CREATE_REVIEW_FAILURE;
+  payload: any;
+}
+
+export interface ProductCreateReviewResetAction {
+  type: ProductCreateReviewActionTypes.PRODUCT_CREATE_REVIEW_RESET;
+}
+
+export type ProductCreateReviewAction =
+  | ProductCreateReviewRequestAction
+  | ProductCreateReviewSuccessAction
+  | ProductCreateReviewFailureAction
+  | ProductCreateReviewResetAction;
