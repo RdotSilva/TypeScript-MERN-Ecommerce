b0d12bd9a14172d17dd35ca97bb648b5987499a7
RdotSilva <RyandotSilva@gmail.com>
2021-04-11 18:58:46 -0400

    Add form for product search

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -7,7 +7,17 @@ const SearchBox = (props: Props) => {
   const [keyword, setKeyword] = useState<string>("");
 
   return (
-    
+      <Form onSubmit={submitHandler} inline>
+          <Form.Control type='text' 
+          name='q' 
+          onChange={(e) => setKeyword(e.target.value)} 
+          placeholder="Search Products..." 
+          className='mr-sm-2 ml-sm-5'>
+          </Form.Control>
+          <Button type='submit' 
+          variant='outline-success' 
+          className='p-2'>Search</Button>
+      </Form>
   )
 
 export default SearchBox;
