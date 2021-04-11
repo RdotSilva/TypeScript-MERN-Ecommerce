1e5995e118d331d1f8e76d84526c75404ebd4adf
RdotSilva <RyandotSilva@gmail.com>
2021-04-11 19:02:09 -0400

    Add submitHandler method

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useHistory } from "react-router";
 
 interface Props {}
 
 const SearchBox = (props: Props) => {
   const [keyword, setKeyword] = useState<string>("");
+  const { push } = useHistory();
+
+  /**
+   * Search for a keyword
+   */
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      push(`/search/${keyword}`);
+    } else {
+      push("/");
+    }
+  };
 
   return (
-      <Form onSubmit={submitHandler} inline>
-          <Form.Control type='text' 
-          name='q' 
-          onChange={(e) => setKeyword(e.target.value)} 
-          placeholder="Search Products..." 
-          className='mr-sm-2 ml-sm-5'>
-          </Form.Control>
-          <Button type='submit' 
-          variant='outline-success' 
-          className='p-2'>Search</Button>
-      </Form>
-  )
+    <Form onSubmit={submitHandler} inline>
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button type="submit" variant="outline-success" className="p-2">
+        Search
+      </Button>
+    </Form>
+  );
+};
 
 export default SearchBox;
