ec64fd0818be342b031a8a38ea218c4307e37d09
RdotSilva <RyandotSilva@gmail.com>
2021-04-11 18:57:55 -0400

    Add SearchBox component

---
+++ frontend/src/components/SearchBox.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+interface Props {}
+
+const SearchBox = (props: Props) => {
+  const [keyword, setKeyword] = useState<string>("");
+
+  return (
+    
+  )
+
+export default SearchBox;
