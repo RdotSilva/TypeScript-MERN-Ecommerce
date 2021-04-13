1a5f434cfecca61e26582d86e7aecb73381db65c
RdotSilva <RyandotSilva@gmail.com>
2021-04-13 19:37:26 -0400

    Add Pagination links

--- frontend/src/components/Paginate.tsx
+++ frontend/src/components/Paginate.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { Pagination } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 interface Props {
   pages: number;
-  page: string;
-  isAdmin: boolean;
-  keyword: string;
+  page: number;
+  isAdmin?: boolean;
+  keyword?: string;
 }
 
 const Paginate = ({ pages, page, isAdmin = false, keyword = "" }: Props) => {
-  return;
+  return (
+    pages > 1 && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => (
+          <LinkContainer
+            key={x + 1}
+            to={keyword ? `/search/${keyword}/page/${x + 1}` : `/page/${x + 1}`}
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>
+        ))}
+      </Pagination>
+    )
+  );
 };
 
 export default Paginate;
