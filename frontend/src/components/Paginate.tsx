a837d64391c8c2903c4df14ecbf285b2924b446f
RdotSilva <RyandotSilva@gmail.com>
2021-04-14 18:16:57 -0400

    Update logic to handle admin

--- frontend/src/components/Paginate.tsx
+++ frontend/src/components/Paginate.tsx
@@ -17,7 +17,11 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = "" }: Props) => {
             <LinkContainer
               key={x + 1}
               to={
-                keyword ? `/search/${keyword}/page/${x + 1}` : `/page/${x + 1}`
+                !isAdmin
+                  ? keyword
+                    ? `/search/${keyword}/page/${x + 1}`
+                    : `/page/${x + 1}`
+                  : `/admin/productlist/${x + 1}`
               }
             >
               <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
