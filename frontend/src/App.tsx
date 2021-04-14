2fdef602706d0c9206b875047daf0888b6b020ae
RdotSilva <RyandotSilva@gmail.com>
2021-04-14 18:18:15 -0400

    Add admin produclist pagination route to main App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,16 @@ const App: React.FC = () => {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
-          <Route path="/admin/productlist" component={ProductListScreen} />
+          <Route
+            path="/admin/productlist"
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path="/admin/productlist:pageNumber"
+            component={ProductListScreen}
+            exact
+          />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
