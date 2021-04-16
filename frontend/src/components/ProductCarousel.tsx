5307670900e5957fa3ccd98a36e7eea8f1efa0c0
RdotSilva <RyandotSilva@gmail.com>
2021-04-16 19:19:38 -0400

    Add productTopRated Redux state

--- frontend/src/components/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ReduxState } from "../types/ReduxState";
 
 interface Props {}
 
 const ProductCarousel = (props: Props) => {
+  const dispatch = useDispatch();
+
+  const { loading, error, products } = useSelector(
+    (state: ReduxState) => state.productTopRated
+  );
+  
   return <div>Product Carousel</div>;
 };
 
