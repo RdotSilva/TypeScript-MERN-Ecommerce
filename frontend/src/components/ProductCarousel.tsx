b2367482ac1091956dc6e8abc63aacb4fc3e4222
RdotSilva <RyandotSilva@gmail.com>
2021-04-17 17:38:33 -0400

    Add Carousel container

--- frontend/src/components/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
+import { Carousel, Image } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { listTopProducts } from "../actions/productActions";
 import { ReduxState } from "../types/ReduxState";
+import Loader from "./Loader.";
+import Message from "./Message";
 
 interface Props {}
 
@@ -19,7 +23,16 @@ const ProductCarousel = (props: Props) => {
     dispatch(listTopProducts());
   }, [dispatch]);
 
-  return <div>Product Carousel</div>;
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <Carousel pause="hover" className="bg-dark">
+      
+      ))}
+    </Carousel>
+  );
 };
 
 export default ProductCarousel;
