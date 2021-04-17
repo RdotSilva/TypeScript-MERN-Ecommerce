818dd14a5d65e85c166d3551e3216986ba67db18
RdotSilva <RyandotSilva@gmail.com>
2021-04-17 17:38:43 -0400

    Update logic to map through products

--- frontend/src/components/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel.tsx
@@ -29,7 +29,17 @@ const ProductCarousel = (props: Props) => {
     <Message variant="danger">{error}</Message>
   ) : (
     <Carousel pause="hover" className="bg-dark">
-      
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/product/${product._id}`}>
+            <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Caption className="carousel-caption">
+              <h2>
+                {product.name} ({product.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
       ))}
     </Carousel>
   );
