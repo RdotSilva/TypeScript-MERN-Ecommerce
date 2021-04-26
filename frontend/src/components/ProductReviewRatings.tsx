1177fdda767c245ed1f1d700092ff8b7a758d3de
RdotSilva <RyandotSilva@gmail.com>
2021-04-26 19:04:57 -0400

    Add useEffect hook to calculate totals for ratings

--- frontend/src/components/ProductReviewRatings.tsx
+++ frontend/src/components/ProductReviewRatings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, ListGroup } from "react-bootstrap";
 import { Review } from "../types";
 import Rating from "./Rating";
@@ -8,11 +8,33 @@ interface Props {
 }
 
 const ProductReviewRatings = ({ productReviews }: Props) => {
-  const [oneStarReviews, setOneStartReviews] = useState(null);
-  const [twoStarReviews, setTwoStarReviews] = useState(null);
-  const [threeStarReviews, setThreeStarReviews] = useState(null);
-  const [fourStarReviews, setFourStarReviews] = useState(null);
-  const [fiveStarReviews, setFiveStarReviews] = useState(null);
+  const [oneStarReviews, setOneStartReviews] = useState(0);
+  const [twoStarReviews, setTwoStarReviews] = useState(0);
+  const [threeStarReviews, setThreeStarReviews] = useState(0);
+  const [fourStarReviews, setFourStarReviews] = useState(0);
+  const [fiveStarReviews, setFiveStarReviews] = useState(0);
+
+  useEffect(() => {
+    productReviews.forEach((review) => {
+      switch (review.rating) {
+        case 1:
+          setOneStartReviews(oneStarReviews + 1);
+          break;
+        case 2:
+          setTwoStarReviews(twoStarReviews + 1);
+          break;
+        case 3:
+          setThreeStarReviews(threeStarReviews + 1);
+          break;
+        case 4:
+          setFourStarReviews(fourStarReviews + 1);
+          break;
+        case 5:
+          setFiveStarReviews(fiveStarReviews + 1);
+          break;
+      }
+    });
+  }, []);
 
   return (
     <>
