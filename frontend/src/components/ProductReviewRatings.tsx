e0f4b197f34a0175af56dd3276984434a46fe357
RdotSilva <RyandotSilva@gmail.com>
2021-04-26 18:57:35 -0400

    Add starReview state

--- frontend/src/components/ProductReviewRatings.tsx
+++ frontend/src/components/ProductReviewRatings.tsx
@@ -8,6 +8,12 @@ interface Props {
 }
 
 const ProductReviewRatings = ({ productReviews }: Props) => {
+  const [oneStarReviews, setOneStartReviews] = useState(null);
+  const [twoStarReviews, setTwoStarReviews] = useState(null);
+  const [threeStarReviews, setThreeStarReviews] = useState(null);
+  const [fourStarReviews, setFourStarReviews] = useState(null);
+  const [fiveStarReviews, setFiveStarReviews] = useState(null);
+
   return (
     <>
       {productReviews.map((review) => (
