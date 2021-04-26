a14a99c92a7fac363b34e70010117a5051d12aee
RdotSilva <RyandotSilva@gmail.com>
2021-04-26 18:44:00 -0400

    Add ProductReviewRatings component

---
+++ frontend/src/components/ProductReviewRatings.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Review } from "../types";
+
+const ProductReviewRatings = ({ productReviews }) => {
+  return <div></div>;
+};
+
+export default ProductReviewRatings;
