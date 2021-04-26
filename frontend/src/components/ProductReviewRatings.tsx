3d125591d53212e4bcf1b4f6de21f815e6e5892e
RdotSilva <RyandotSilva@gmail.com>
2021-04-26 18:56:34 -0400

    Add logic to create review rating card

--- frontend/src/components/ProductReviewRatings.tsx
+++ frontend/src/components/ProductReviewRatings.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, ListGroup } from "react-bootstrap";
 import { Review } from "../types";
+import Rating from "./Rating";
 
 interface Props {
   productReviews: Review[];
 }
 
 const ProductReviewRatings = ({ productReviews }: Props) => {
-  return <div></div>;
+  return (
+    <>
+      {productReviews.map((review) => (
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Rating value={review.rating} />
+            </ListGroup.Item>
+            <ListGroup.Item>{productReviews.length}</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      ))}
+    </>
+  );
 };
 
 export default ProductReviewRatings;
