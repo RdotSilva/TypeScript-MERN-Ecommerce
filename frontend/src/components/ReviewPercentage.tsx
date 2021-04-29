1b4f21b4aeedb12d1c7b50d7d197d304d85319f9
RdotSilva <RyandotSilva@gmail.com>
2021-04-29 18:51:54 -0400

    Move calculateReviewRatingPercentage method

--- frontend/src/components/ReviewPercentage.tsx
+++ frontend/src/components/ReviewPercentage.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import { Review } from "../types";
+interface Props {
+  productReviews?: Review[];
+  totalReviews?: number;
+  reviewsPerStar?: number;
+}
 
-interface Props {}
+const ReviewPercentage = ({ reviewsPerStar, totalReviews }: Props) => {
+  /**
+   * Calculates the total percentage of reviews per star based on the total reviews
+   */
+  const calculateReviewRatingPercentage = (
+    reviewsPerRating: number,
+    totalReviews: number
+  ) => {
+    return Math.round((100 * reviewsPerRating) / totalReviews);
+  };
 
-const ReviewPercentage = (props: Props) => {
-  return <div>Review Percentage</div>;
+  return (
+    
+  );
 };
 
 export default ReviewPercentage;
