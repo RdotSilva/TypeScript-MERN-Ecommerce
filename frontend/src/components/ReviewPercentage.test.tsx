9b7c483ca5c5a9764e9bdde475194ec2279631dc
RdotSilva <RyandotSilva@gmail.com>
2021-05-01 19:14:23 -0400

    Add test for review render

--- frontend/src/components/ReviewPercentage.test.tsx
+++ frontend/src/components/ReviewPercentage.test.tsx
@@ -1,7 +1,16 @@
 import ReactDOM from "react-dom";
+import { getByTestId, getByText, render, screen } from "@testing-library/react";
 import ReviewPercentage from "./ReviewPercentage";
+import { Dropdown } from "react-bootstrap";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<ReviewPercentage />, div);
+});
+
+it("renders review", () => {
+  const { getByText } = render(
+    <ReviewPercentage reviewsPerStar={1} totalReviews={2} />
+  );
+  expect(getByText("review")).toBeVisible();
 });
