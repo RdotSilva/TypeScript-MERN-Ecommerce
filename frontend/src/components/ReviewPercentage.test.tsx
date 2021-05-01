b868e084516e2adaa254e1d57aaefa7d1914dc30
RdotSilva <RyandotSilva@gmail.com>
2021-05-01 18:53:13 -0400

    Add test for render

--- frontend/src/components/ReviewPercentage.test.tsx
+++ frontend/src/components/ReviewPercentage.test.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
+import ReactDOM from "react-dom";
+import ReviewPercentage from "./ReviewPercentage";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<ReviewPercentage />, div);
 });
