c59a826f12dbc7d9c075840a91a060829182d57d
RdotSilva <RyandotSilva@gmail.com>
2021-05-02 15:59:54 -0400

    Update test to use placeholder text

--- frontend/src/components/SearchBox.test.tsx
+++ frontend/src/components/SearchBox.test.tsx
@@ -2,7 +2,8 @@ import { render } from "@testing-library/react";
 import SearchBox from "./SearchBox";
 
 it("renders correctly", () => {
-  const { queryByTestId } = render(<SearchBox />);
+  const { queryByTestId, queryByPlaceholderText } = render(<SearchBox />);
 
   expect(queryByTestId("search-box")).toBeTruthy();
+  expect(queryByPlaceholderText("Searching...")).toBeTruthy();
 });
