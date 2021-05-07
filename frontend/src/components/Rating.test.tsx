f5fc7b106fd2008ef8ddfcdeb2102f1731cae4f9
RdotSilva <RyandotSilva@gmail.com>
2021-05-07 19:27:52 -0400

    Add test for one star review

--- frontend/src/components/Rating.test.tsx
+++ frontend/src/components/Rating.test.tsx
@@ -12,3 +12,11 @@ it("should contain a child span element", () => {
 
   expect(queryByTestId("rating-span")).toBeTruthy();
 });
+
+it("one star review should have one star image class name", () => {
+  const { container } = render(<Rating value={1} />);
+
+  expect(
+    container.firstElementChild?.firstElementChild?.firstElementChild?.className
+  ).toBe("fas fa-star");
+});
