aeb1c2d8dfda7064cd3fc7dd7d1210ee5751ac7f
RdotSilva <RyandotSilva@gmail.com>
2021-05-11 17:42:12 -0400

    Add test for product properties length

--- backend/data/products.test.ts
+++ backend/data/products.test.ts
@@ -7,3 +7,11 @@ it("is an array", () => {
 it("contains 6 products", () => {
   expect(products.length).toEqual(6);
 });
+
+it("each product has 9 properties", () => {
+  const productsWithNineProperties = products.filter(
+    (product) => Object.keys(product).length === 9
+  );
+
+  expect(productsWithNineProperties.length).toEqual(6);
+});
