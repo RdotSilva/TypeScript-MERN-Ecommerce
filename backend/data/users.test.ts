8933aaa6e9a444037932c9a8edc3f53fe9cf336b
RdotSilva <RyandotSilva@gmail.com>
2021-05-11 17:46:19 -0400

    Add test for user properties length

--- backend/data/users.test.ts
+++ backend/data/users.test.ts
@@ -7,3 +7,11 @@ it("is an array", () => {
 it("contains 4 users", () => {
   expect(users.length).toEqual(4);
 });
+
+it("each user has 4 properties", () => {
+  const usersWithFourProperties = users.filter(
+    (product) => Object.keys(product).length === 4
+  );
+
+  expect(usersWithFourProperties.length).toEqual(4);
+});
