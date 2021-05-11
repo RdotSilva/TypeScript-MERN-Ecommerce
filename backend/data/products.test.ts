a563429d20508d3ae64fbace2c0c37e86c2b8639
RdotSilva <RyandotSilva@gmail.com>
2021-05-11 17:38:18 -0400

    Add test for product length

--- backend/data/products.test.ts
+++ backend/data/products.test.ts
@@ -3,3 +3,7 @@ import products from "./products";
 it("is an array", () => {
   expect(Array.isArray(products)).toBeTruthy();
 });
+
+it("contains 6 products", () => {
+  expect(products.length).toEqual(6);
+});
