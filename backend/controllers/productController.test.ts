db8673b2a7d684e7bf27b773b2fd94bc35c91887
RdotSilva <RyandotSilva@gmail.com>
2021-05-12 18:55:28 -0400

    Add test for GET products

--- backend/controllers/productController.test.ts
+++ backend/controllers/productController.test.ts
@@ -7,3 +7,12 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/products/", productRoutes);
+
+describe("GET /api/products", () => {
+  it("responds with 200", (done) => {
+    request(app)
+      .get("/api/products")
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
