422ebe5e1658e1f5a2928b3b5f34634352fd53d7
RdotSilva <RyandotSilva@gmail.com>
2021-05-13 18:17:55 -0400

    Add test for POST user auth

--- backend/controllers/userController.test.ts
+++ backend/controllers/userController.test.ts
@@ -6,4 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/products/", userRoutes);
+app.use("/api/users/", userRoutes);
+
+describe("POST /api/users/login", () => {
+  it("responds with 200", (done) => {
+    request(app)
+      .get("/api/users/login")
+      .set("Accept", "application/json")
+      .expect(200, done);
+  });
+});
