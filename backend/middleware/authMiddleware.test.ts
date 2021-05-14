7934534b62b26f89b4836e121e0895f8ceb02307
RdotSilva <RyandotSilva@gmail.com>
2021-05-14 19:13:27 -0400

    Add mock setup

---
+++ backend/middleware/authMiddleware.test.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+describe("Authorization middleware", () => {
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
+
+  beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {
+      json: jest.fn(),
+    };
+  });
+});
