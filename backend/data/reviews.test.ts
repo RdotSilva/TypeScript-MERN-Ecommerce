7230860226d748ad61687a42ebae10f91683b09e
RdotSilva <RyandotSilva@gmail.com>
2021-05-15 16:53:43 -0400

    Add test for review count

--- backend/data/reviews.test.ts
+++ backend/data/reviews.test.ts
@@ -3,3 +3,7 @@ import reviews from "./reviews";
 it("is an array", () => {
   expect(Array.isArray(reviews)).toBeTruthy();
 });
+
+it("contains 6 reviews", () => {
+  expect(reviews.length).toEqual(6);
+});
