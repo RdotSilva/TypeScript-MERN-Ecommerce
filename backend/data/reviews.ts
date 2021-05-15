6d588814a82ca1b0488aeff7c4a47340f6c8e6c9
RdotSilva <RyandotSilva@gmail.com>
2021-05-15 16:52:06 -0400

    Add reviews test data

---
+++ backend/data/reviews.ts
@@ -0,0 +1,42 @@
+import { Review } from "../types";
+
+const reviews: Review[] = [
+  {
+    user: "abc123",
+    name: "Name One",
+    comment: "Good",
+    rating: 3,
+  },
+  {
+    user: "abc1234",
+    name: "Name Two",
+    comment: "Great",
+    rating: 5,
+  },
+  {
+    user: "abc12345",
+    name: "Name Three",
+    comment: "Bad",
+    rating: 2,
+  },
+  {
+    user: "abc123456",
+    name: "Name Four",
+    comment: "Horrible",
+    rating: 1,
+  },
+  {
+    user: "abc12345678",
+    name: "Name Five",
+    comment: "Okay",
+    rating: 4,
+  },
+  {
+    user: "abc123456789",
+    name: "Name Six",
+    comment: "Subpar",
+    rating: 2,
+  },
+];
+
+export default reviews;
