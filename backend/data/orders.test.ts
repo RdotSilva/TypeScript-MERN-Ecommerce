b1bb5090b7bb117efeeb970c7c765b6b5eb9e08b
RdotSilva <RyandotSilva@gmail.com>
2021-05-16 18:01:51 -0400

    Add test for number of orders

--- backend/data/orders.test.ts
+++ backend/data/orders.test.ts
@@ -3,3 +3,7 @@ import orders from "./orders";
 it("is an array", () => {
   expect(Array.isArray(orders)).toBeTruthy();
 });
+
+it("contains 3 orders", () => {
+  expect(orders.length).toEqual(3);
+});
