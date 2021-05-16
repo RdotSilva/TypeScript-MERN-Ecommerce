b7079064b41d94470ca067b853441c086873d90d
RdotSilva <RyandotSilva@gmail.com>
2021-05-16 18:02:56 -0400

    Add test for order properties

--- backend/data/orders.test.ts
+++ backend/data/orders.test.ts
@@ -7,3 +7,11 @@ it("is an array", () => {
 it("contains 3 orders", () => {
   expect(orders.length).toEqual(3);
 });
+
+it("each order has 12 properties", () => {
+  const ordersWithTwelveProperties = orders.filter(
+    (order) => Object.keys(order).length === 12
+  );
+
+  expect(ordersWithTwelveProperties.length).toEqual(3);
+});
